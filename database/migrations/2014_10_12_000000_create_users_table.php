85ddbc66832e5fde4db104d06243173f8bbdf46e
BePsvPT <freedom@csie.io>
2016-03-12 19:45:32 +0800

    Remove password resets migration and update users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,10 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
+            $table->string('username', 24)->unique();
+            $table->string('password', 100);
+            $table->string('nickname', 16)->unique();
+            $table->string('email', 48)->unique();
             $table->rememberToken();
             $table->timestamps();
         });
