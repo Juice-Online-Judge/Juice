0149aed9c1b5aeeac0a6edfc450de71ccd1393eb
BePsvPT <freedom@csie.io>
2016-03-12 23:12:00 +0800

    Create seeders

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,21 +1,45 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
-
-$factory->define(App\User::class, function (Faker\Generator $faker) {
+$factory->define(App\Models\User\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->email,
+        'username' => $faker->userName,
         'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
+        'nickname' => $faker->name,
+        'email' => $faker->email,
+    ];
+});
+
+$factory->define(App\Models\Question::class, function (Faker\Generator $faker) {
+    return [
+        'title' => $faker->sentence,
+        'description' => $faker->paragraph,
+        'test_data' => $faker->sentence,
+        'restriction' => [
+            'time' => $faker->randomFloat(null, 1.0, 10.0),
+            'memory' => $faker->randomFloat(null, 1.0, 100.0),
+            'file' => $faker->numberBetween(0, 10),
+            'library' => $faker->randomElement(['strstr', 'strlen', 'malloc']),
+        ],
+        'public' => $faker->boolean(),
+    ];
+});
+
+$factory->define(App\Models\Submission::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => \App\Models\User\User::all(['id'])->random()->getAttribute('id'),
+        'question_id' => \App\Models\Question::all(['id'])->random()->getAttribute('id'),
+        'code' => $faker->paragraph,
+        'submitted_at' => $faker->dateTime,
+    ];
+});
+
+$factory->define(App\Models\Judge::class, function (Faker\Generator $faker) {
+    return [
+        'result' => $faker->randomElement(['AC', 'WA', 'SE', 'CE', 'RE', 'TLE', 'MLE', 'EOF']),
+        'compiled_info' => $faker->boolean() ? $faker->sentence : null,
+        'time' => $faker->randomFloat(null, 1.0, 10.0),
+        'memory' => $faker->randomFloat(null, 1.0, 100.0),
+        'file' => $faker->numberBetween(0, 10),
+        'judged_at' => $faker->dateTime,
     ];
 });
---
+++ database/seeds/SubmissionsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\Judge;
+use App\Models\Submission;
+use Illuminate\Database\Seeder;
+
+class SubmissionsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Submission::class, 20)->create()->each(function (Submission $submission) {
+            $submission->judge()->save(factory(Judge::class)->create([
+                'submission_id' => $submission->getAttribute('id'),
+            ]));
+        });
+    }
+}
