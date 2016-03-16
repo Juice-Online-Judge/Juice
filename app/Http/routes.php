a8ef6b3ed318b28b2fc1ee6c6aed05c6b8f35fb3
BePsvPT <freedom@csie.io>
2016-03-16 13:31:03 +0800

    Update routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -4,14 +4,16 @@ use Illuminate\Routing\Router;
 
 /* @var Router $router */
 
-$router->get('/', ['middleware' => 'web', 'uses' => 'HomeController@home']);
-
-$router->group(['prefix' => 'api', 'namespace' => 'Api', 'middleware' => ['web']], function (Router $router) {
-    $router->group(['prefix' => 'v1', 'namespace' => 'V1'], function (Router $router) {
-        $router->group(['prefix' => 'auth', 'middleware' => ['throttle:5,1']], function (Router $router) {
-            $router->get('sign-in', 'AuthController@signIn');
-            $router->get('sign-out', 'AuthController@signOut');
-            $router->get('sign-up', 'AuthController@signUp');
+$router->group(['middleware' => ['web']], function (Router $router) {
+    $router->group(['prefix' => 'api', 'namespace' => 'Api'], function (Router $router) {
+        $router->group(['prefix' => 'v1', 'namespace' => 'V1'], function (Router $router) {
+            $router->group(['prefix' => 'auth', 'middleware' => ['throttle:5,1']], function (Router $router) {
+                $router->post('sign-in', 'AuthController@signIn');
+                $router->get('sign-out', 'AuthController@signOut');
+                $router->post('sign-up', 'AuthController@signUp');
+            });
         });
     });
+
+    $router->get('{redirect}', 'HomeController@home')->where('redirect', '.*');
 });
