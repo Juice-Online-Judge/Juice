41755612879ca641f0bfe74b5c6f7bd8858436f5
DanSnow <dododavid006@gmail.com>
2016-03-16 12:29:09 +0800

    Fetch csrf token

--- src/lib/api.js
+++ src/lib/api.js
@@ -9,6 +9,9 @@ const api = rest
   .wrap(pathPrefix, { prefix: '/api/v1' })
   .wrap(mime, { mime: 'application/json' })
   .wrap(errorCode)
-  .wrap(csrf, { token: Cookie.get('csrf-token') });
+  .wrap(csrf, {
+    name: 'X-XSRF-TOKEN',
+    token: Cookie.get('XSRF-TOKEN')
+  });
 
 export default api;
