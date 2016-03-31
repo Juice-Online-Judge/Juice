747c77acd079f38ea9eefa61869262484acc3a2f
DanSnow <dododavid006@gmail.com>
2016-03-31 22:37:41 +0800

    Update question.js

--- bin/server.js
+++ bin/server.js
@@ -1,11 +1,37 @@
 require('babel-register');
 
+const express = require('express');
+const url = require('url');
 const config = require('../config');
-const server = require('../server/main');
 const debug = require('debug')('app:bin:server');
+const proxy = require('proxy-middleware');
+const webpack = require('webpack');
+const webpackConfig = require('../build/webpack.config');
+const WebpackDevServer = require('webpack-dev-server');
 
 const port = config.server_port;
 const host = config.server_host;
+const paths = config.utils_paths;
 
-server.listen(port);
-debug(`Server is now running at ${host}:${port}.`);
+const publicPath = webpackConfig.output.publicPath;
+
+const compiler = webpack(webpackConfig);
+const app = new WebpackDevServer(compiler, {
+  publicPath,
+  contentBase: paths.base(config.dir_client),
+  hot: true,
+  quiet: config.compiler_quiet,
+  noInfo: config.compiler_quiet,
+  lazy: false,
+  stats: config.compiler_stats,
+  proxy: {
+    '/*': {
+      target: 'http://localhost:8000'
+    }
+  }
+});
+
+// Serve static assets from ~/src/static since Webpack is unaware of
+// these files. This middleware doesn't need to be enabled outside
+// of development since this directory will be copied into ~/dist
+app.listen(port);
--- src/redux/modules/question.js
+++ src/redux/modules/question.js
@@ -1,7 +1,53 @@
 import { createAction, handleActions } from 'redux-actions';
-import Immutable from 'immutable';
+import { Record, List } from 'immutable';
 import api from 'lib/api';
 
-const initialState = Immutable.fromJS({
-  list: []
+const QuestionState = new Record({
+  list: new List()
+  loading: true,
+  page: 0,
+  total: 0,
+  error: null
 });
+
+const initialState = new QuestionState();
+
+const SET_QUESTION = 'SET_QUESTION';
+const SET_LOADING = 'SET_LOADING';
+const SET_ERROR = 'SET_ERROR';
+
+export const setQuestion = createAction(SET_QUESTION);
+export const setLoading = createAction(SET_LOADING);
+export const setError = createAction(SET_ERROR);
+
+export const fetchQuestion(query = { page: 1 }) => {
+  return (dispatch) => {
+    dispatch(setLoading(true));
+    api({
+      path: 'questions',
+      params: query
+    })
+    .entity()
+    .then((entity) => {
+      dispatch(setQuestion({
+        list: entity.data,
+        page: query.page,
+        total: entity.total
+      }));
+      dispatch(setLoading(false));
+    })
+    .catch((error) => {
+      dispatch(setLoading(false));
+      dispatch(setError('Server error'));
+      if(error instanceof Error) {
+        throw error;
+      }
+    });
+  };
+}
+
+export default handleActions({
+  [SET_QUESTION]: (state, { payload }) => state.merge(payload),
+  [SET_LOADING]: (state, { payload }) => state.set('loading', payload),
+  [SET_ERROR]: (state, { payload }) => state.set('error', payload)
+}, initialState);
