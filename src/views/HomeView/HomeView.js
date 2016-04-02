7e4454136a98133fd1519cefc5aeb883eb716ff5
DanSnow <dododavid006@gmail.com>
2016-04-02 15:37:17 +0800

    Show questions

--- src/views/HomeView/HomeView.js
+++ src/views/HomeView/HomeView.js
@@ -1,50 +1,45 @@
 import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 
-// We define mapStateToProps where we'd normally use
-// the @connect decorator so the data requirements are clear upfront, but then
-// export the decorated component after the main class definition so
-// the component can be tested w/ and w/o being connected.
-// See: http://rackt.github.io/redux/docs/recipes/WritingTests.html
-export class HomeView extends React.Component {
-  static propTypes = {
-    loginState: PropTypes.object.isRequired
-  };
+import { actions as questionActions } from 'redux/modules/question';
 
-  get welcome() {
-    let { loginState } = this.props;
-    if (loginState.get('valid')) {
-      if (loginState.get('state')) {
-        return (
-          <h1>
-            Welcome user: { loginState.getIn(['user', 'username']) }
-          </h1>
-        );
-      } else {
-        return (
-          <h1>
-            Please login
-          </h1>
-        );
-      }
-    } else {
+import Question from 'components/Question';
+import CenterLoading from 'components/CenterLoading';
+
+export class HomeView extends React.Component {
+  componentDidMount() {
+    this.props.fetchQuestion();
+  }
+
+  get questionList() {
+    const { question } = this.props;
+    if (question.get('loading')) {
       return (
-        <h1>
-          Loading...
-        </h1>
+        <CenterLoading loading />
       );
+    } else {
+      return question.get('list').map((ques, idx) => {
+        return (
+          <Question question={ ques } key={ idx } />
+        );
+      });
     }
   }
 
   render() {
     return (
       <div>
-        { this.welcome }
+        { this.questionList }
       </div>
     );
   }
 }
 
+HomeView.propTypes = {
+  question: PropTypes.object.isRequired,
+  fetchQuestion: PropTypes.func.isRequired
+};
+
 export default connect((state) => {
-  return { loginState: state.auth };
-})(HomeView);
+  return { question: state.question };
+}, questionActions)(HomeView);
