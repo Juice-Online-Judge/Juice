77e0546bcc7283746863143ef2fd6346177cddcb
DanSnow <dododavid006@gmail.com>
2016-04-02 15:36:47 +0800

    Add question to store

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { routerReducer as routing } from 'react-router-redux';
 import auth from './modules/auth';
+import question from './modules/question';
 
 export default combineReducers({
   auth,
+  question,
   routing
 });
