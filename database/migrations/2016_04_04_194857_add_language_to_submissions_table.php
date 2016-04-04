fe5cc5c088475fec6bf6e8c7149c4b2d4534834c
BePsvPT <freedom@csie.io>
2016-04-04 20:33:35 +0800

    Add language column to submissions table

---
+++ database/migrations/2016_04_04_194857_add_language_to_submissions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddLanguageToSubmissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            $table->string('language', 16)->index()->after('question_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('submissions', function (Blueprint $table) {
+            $table->dropColumn('language');
+        });
+    }
+}
