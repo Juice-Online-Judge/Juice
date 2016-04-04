eb8b98752c4e23989722bf8c3d775613ca590245
DanSnow <dododavid006@gmail.com>
2016-04-04 21:29:57 +0800

    Show filename

--- src/components/FileButton.js
+++ src/components/FileButton.js
@@ -2,11 +2,18 @@ import React, { Component, PropTypes } from 'react';
 import { autobind } from 'core-decorators';
 
 import FlatButton from 'material-ui/lib/flat-button';
+import TextField from 'material-ui/lib/text-field';
 
 export class FileButton extends Component {
   @autobind
   handleChange(event) {
     const files = event.target.files;
+    if (files.length) {
+      this.setState({ filename: files[0].name });
+    } else {
+      this.setState({ filename: null });
+    }
+
     if (this.props.onChange) {
       this.props.onChange(files);
     }
@@ -14,11 +21,18 @@ export class FileButton extends Component {
 
   render() {
     return (
-      <FlatButton label={ this.props.label } labelPosition='before'>
-        <input type='file' style={ styles.file } onChange={ this.handleChange } />
-      </FlatButton>
+      <div>
+        <FlatButton primary label={ this.props.label } labelPosition='before'>
+          <input type='file' style={ styles.file } onChange={ this.handleChange } />
+        </FlatButton>
+        <TextField hintText='Please select file' value={ this.state.filename } />
+      </div>
     );
   }
+
+  state = {
+    filename: null
+  };
 
   static propTypes = {
     label: PropTypes.string.isRequired,
