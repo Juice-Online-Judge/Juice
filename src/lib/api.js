fd27ca40402539fc807820607aa32642d64bcd42
DanSnow <dododavid006@gmail.com>
2016-04-06 10:51:29 +0800

    Add accpet type config

--- src/lib/api.js
+++ src/lib/api.js
@@ -7,7 +7,10 @@ import Cookie from 'js-cookie';
 
 const api = rest
   .wrap(pathPrefix, { prefix: '/api/v1' })
-  .wrap(mime, { mime: 'application/json' })
+  .wrap(mime, {
+    mime: 'application/json',
+    accept: 'application/json;q=0.8, text/plain;q=0.5, */*;q=0.2'
+  })
   .wrap(errorCode)
   .wrap(csrf, {
     name: 'X-XSRF-TOKEN',
