c161143fb0d0125cc29440823675167a27371b39
DanSnow <dododavid006@gmail.com>
2016-04-14 15:00:20 +0800

    Fix layout

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -1,11 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 
-import { Grid, Row, Col } from 'react-flexbox-grid';
+import { Row, Col } from 'react-flexbox-grid';
 import { Link } from 'react-router';
 import Card from 'material-ui/lib/card/card';
 import CardText from 'material-ui/lib/card/card-text';
 import IconButton from 'material-ui/lib/icon-button';
-import CodeIcon from  'material-ui/lib/svg-icons/action/code';
+import CodeIcon from 'material-ui/lib/svg-icons/action/code';
 
 class Submission extends Component {
   render() {
@@ -20,33 +20,31 @@ class Submission extends Component {
     return (
       <Card>
         <CardText style={ styles.padding }>
-          <Grid>
-            <Row middle='xs'>
-              <Col xs={ 6 }>
-                <Link to={ `/question/${quesUuid}` }>
-                  { title }
-                </Link>
-              </Col>
-              <Col xs={ 1 } /> { /* Use for padding */ }
-              <Col xs={ 1 } style={ result === 'AC' ? styles.pass : styles.noPass }>
-                { result }
-              </Col>
-              <Col style={ styles.upperCase } xs={ 1 }>
-                { language }
-              </Col>
-              <Col xs={ 1 }>
-                { time } (s)
-              </Col>
-              <Col xs={ 1 }>
-                { memory } (KB)
-              </Col>
-              <Col xs={ 1 }>
-                <IconButton tooltip='View Code'>
-                  <CodeIcon />
-                </IconButton>
-              </Col>
-            </Row>
-          </Grid>
+          <Row middle='xs'>
+            <Col xs={ 6 }>
+              <Link to={ `/question/${quesUuid}` }>
+                { title }
+              </Link>
+            </Col>
+            <Col xs={ 1 } /> { /* Use for padding */ }
+            <Col xs={ 1 } style={ result === 'AC' ? styles.pass : styles.noPass }>
+              { result }
+            </Col>
+            <Col style={ styles.upperCase } xs={ 1 }>
+              { language }
+            </Col>
+            <Col xs={ 1 }>
+              { time } (s)
+            </Col>
+            <Col xs={ 1 }>
+              { memory } (KB)
+            </Col>
+            <Col xs={ 1 }>
+              <IconButton tooltip='View Code'>
+                <CodeIcon />
+              </IconButton>
+            </Col>
+          </Row>
         </CardText>
       </Card>
     );
