0ebc0a11e4e9ddc26c49bfde0a8390037cb66074
DanSnow <dododavid006@gmail.com>
2016-04-14 13:03:06 +0800

    Update config

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,10 +1,35 @@
 const path = require('path');
 
+const cssLoader = [
+    'css?modules',
+    'sourceMap',
+    'importLoaders=1',
+    'localIdentName=[name]__[local]___[hash:base64:5]'
+  ].join('&');
+
 module.exports = {
   module: {
     resolve: {
       root: path.resolve(__dirname, '../src'),
       extensions: ['', '.js', '.jsx']
-    }
+    },
+    loaders: [{
+      test: /\.scss$/,
+      include: /(src|flexboxgrid)/,
+      loaders: [
+        'style',
+        cssLoader,
+        'postcss',
+        'sass'
+      ]
+    }, {
+      test: /\.css$/,
+      include: /(src|flexboxgrid)/,
+      loaders: [
+        'style',
+        cssLoader,
+        'postcss'
+      ]
+    }]
   }
 };
