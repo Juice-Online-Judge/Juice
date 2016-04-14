7d61f9ebdf66fc249266b122904e427f21124fa3
BePsvPT <freedom@csie.io>
2016-04-14 10:53:41 +0800

    Update exam policy

--- app/Policies/Api/V1/ExamPolicy.php
+++ app/Policies/Api/V1/ExamPolicy.php
@@ -26,13 +26,37 @@ class ExamPolicy
      */
     public function show(User $user, Exam $exam)
     {
+        return $this->auth($user, $exam);
+    }
+
+    /**
+     * 確認使用者有權限查詢該測驗.
+     *
+     * @param User $user
+     * @param Exam $exam
+     * @return bool
+     */
+    public function submissions(User $user, Exam $exam)
+    {
+        return $this->auth($user, $exam);
+    }
+
+    /**
+     * 確認使用者有權限查詢該測驗.
+     *
+     * @param User $user
+     * @param Exam $exam
+     * @return bool
+     */
+    protected function auth(User $user, Exam $exam)
+    {
         $allow = $exam->getAttribute('user_id') === $user->getAuthIdentifier();
 
         if ($allow) {
             return true;
         }
 
-        $role = $exam->getRelation('role');
+        $role = $exam->load(['role'])->getRelation('role');
 
         if (is_null($role)) {
             return false;
