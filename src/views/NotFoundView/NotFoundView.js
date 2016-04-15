c19fca177d29d1fb17bc9ee81d5a82db9ab736eb
DanSnow <dododavid006@gmail.com>
2016-04-15 12:54:47 +0800

    Update NotFoundView

--- src/views/NotFoundView/NotFoundView.js
+++ src/views/NotFoundView/NotFoundView.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router';
 export class NotFoundView extends React.Component {
   render() {
     return (
-      <div className='container text-center'>
+      <div>
         <h1>404 Not found</h1>
         <hr />
         <Link to='/'>Back To Index</Link>
