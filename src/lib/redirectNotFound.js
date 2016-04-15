bb401ef6b6a6699996d93d593b9b04c434738a53
DanSnow <dododavid006@gmail.com>
2016-04-15 11:56:13 +0800

    Add redirectNotFound.js

---
+++ src/lib/redirectNotFound.js
@@ -0,0 +1,37 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { replace } from 'react-router-redux';
+
+import { RequestStatus } from 'lib/const';
+import { actions as appActions } from 'redux/modules/app';
+
+export const redirectNotFound = (WrappedComponent) => {
+  class RedirectNotFound extends Component {
+    componentWillReceiveProps(newProps) {
+      if (newProps.app.get('status') === RequestStatus.FAIL &&
+          newProps.app.getIn(['error', 'code']) === 404) {
+        this.props.clearError();
+        this.props.replace('/page-not-found');
+      }
+    }
+
+    render() {
+      const props = this.props;
+
+      return (
+        <WrappedComponent { ...props } />
+      );
+    }
+
+    static propTypes = {
+      app: PropTypes.object.isRequired,
+      clearError: PropTypes.func.isRequired,
+      replace: PropTypes.func.isRequired
+    };
+  }
+
+  return connect((state) => ({ app: state.app }),
+    Object.assign({}, appActions, { replace }))(RedirectNotFound);
+};
+
+export default redirectNotFound;
