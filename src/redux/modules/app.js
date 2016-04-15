d9bcea096f57d6dccc20b2d8c5e3cf61e050a875
DanSnow <dododavid006@gmail.com>
2016-04-15 11:55:44 +0800

    Add clearError

--- src/redux/modules/app.js
+++ src/redux/modules/app.js
@@ -13,19 +13,23 @@ const initialState = new AppStatus();
 const SET_STATUS = 'SET_STATUS';
 const SET_ERROR = 'SET_ERROR';
 const CLEAR_STATUS = 'CLEAR_STATUS';
+const CLEAR_ERROR = 'CLEAR_ERROR';
 
 export const setStatus = createAction(SET_STATUS);
 export const setError = createAction(SET_ERROR);
 export const clearStatus = createAction(CLEAR_STATUS);
+export const clearError = createAction(CLEAR_ERROR);
 
 export const actions = {
   setStatus,
   setError,
-  clearStatus
+  clearStatus,
+  clearError
 };
 
 export default handleActions({
   [SET_STATUS]: (state, { payload }) => state.set('status', payload),
   [CLEAR_STATUS]: (state) => state.set('status', RequestStatus.NONE),
-  [SET_ERROR]: (state, { payload }) => state.set('error', Immutable.fromJS(payload))
+  [SET_ERROR]: (state, { payload }) => state.set('error', Immutable.fromJS(payload)),
+  [CLEAR_ERROR]: (state, { payload }) => state.set('error', null).set('status', RequestStatus.NONE)
 }, initialState);
