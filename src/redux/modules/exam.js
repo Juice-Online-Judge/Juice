69c88b7ebf7c45cb528d7ce5c9eaad8a2a18c6fc
DanSnow <dododavid006@gmail.com>
2016-04-16 11:09:43 +0800

    Add exam

---
+++ src/redux/modules/exam.js
@@ -0,0 +1,55 @@
+import { createAction, handleActions } from 'redux-actions';
+import { Record, Map, List } from 'immutable';
+import { normalize, arrayOf } from 'normalizr';
+
+import api from 'lib/api';
+import examSchema from 'schema/exam';
+import { RequestStatus } from 'lib/const';
+import { setStatus } from './app';
+import handleRequestError from '../utils/handleRequestError';
+
+const ExamStatus = new Record({
+  result: new List(),
+  entities: new Map(),
+  page: 1,
+  total: 0
+});
+
+const initialState = new ExamStatus();
+
+export const SET_EXAM = 'SET_EXAM';
+
+export const setExam = createAction(SET_EXAM);
+
+export const fetchExams = (query, opts = { force: false }) => {
+  return (dispatch, getState) => {
+    const { exam } = getState();
+    const page = exam.get('page');
+    query = query || { page };
+
+    if (exam.get('result').size && query.page === page && !opts.force) {
+      return;
+    }
+
+    dispatch(setStatus(RequestStatus.PENDING));
+    api({
+      path: 'exams',
+      query
+    })
+    .then(({ entity }) => {
+      const data = normalize(entity.data, arrayOf(examSchema));
+      dispatch(setExam({ page: query.page, total: entity.total, ...data }));
+      dispatch(setStatus(RequestStatus.SUCCESS));
+    })
+    .catch(handleRequestError.bind(null, dispatch));
+  };
+};
+
+export const actions = {
+  setExam,
+  fetchExams
+};
+
+export default handleActions({
+  [SET_EXAM]: (state, { payload }) => state.merge(payload)
+}, initialState);
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -5,12 +5,14 @@ import account from './modules/account';
 import question from './modules/question';
 import submission from './modules/submission';
 import validate from './modules/validate';
+import exam from './modules/exam';
 
 export default combineReducers({
   app,
   account,
   question,
   submission,
+  exam,
   validate,
   routing
 });
