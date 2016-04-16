c75147892ed6f7e00db484f499ac8d47da0b7d9d
DanSnow <dododavid006@gmail.com>
2016-04-16 13:51:07 +0800

    Add pagitiate

--- src/views/ExamListView/ExamListView.js
+++ src/views/ExamListView/ExamListView.js
@@ -3,36 +3,55 @@ import { connect } from 'react-redux';
 
 import Inset from 'layouts/Inset';
 import ExamCard from 'components/ExamCard';
+import Pagination from 'components/Pagination';
 
 import { actions as examActions } from 'redux/modules/exam';
 
 class ExamListView extends Component {
   componentDidMount() {
-    this.props.fetchExams();
+    const { query } = this.props.location;
+    this.fetchExams(query);
+  }
+
+  componentWillReceiveProps(newProps) {
+    const { query } = newProps.location;
+    this.fetchExams(query);
+  }
+
+  fetchExams(query) {
+    const page = parseInt(query.page) || 1;
+    this.props.fetchExams({ page });
   }
 
   render() {
     const { exam } = this.props;
     const examData = exam.getIn(['entities', 'exam']);
     return (
-      <Inset>
-        {
-          exam.get('result').map((id) => {
-            return (
-              <ExamCard
-                id={ id }
-                key={ id }
-                name={ examData.getIn([`${id}`, 'name']) }
-                beganTime={ examData.getIn([`${id}`, 'began_at']) }
-                endedTime={ examData.getIn([`${id}`, 'ended_at']) }/>
-            );
-          })
-        }
-      </Inset>
+      <div>
+        <Inset>
+          {
+            exam.get('result').map((id, idx) => {
+              return (
+                <ExamCard
+                  id={ id }
+                  key={ idx }
+                  name={ examData.getIn([`${id}`, 'name']) }
+                  beganTime={ examData.getIn([`${id}`, 'began_at']) }
+                  endedTime={ examData.getIn([`${id}`, 'ended_at']) }/>
+              );
+            })
+          }
+        </Inset>
+        <Pagination
+          baseUrl='/exams'
+          current={ exam.get('page') }
+          maxPage={ Math.ceil(exam.get('total') / 10) }/>
+      </div>
     );
   }
 
   static propTypes = {
+    location: PropTypes.object.isRequired,
     exam: PropTypes.object.isRequired,
     fetchExams: PropTypes.func.isRequired
   };
