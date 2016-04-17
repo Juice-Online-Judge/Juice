cf8cd6baf5e96fb6bca176027f369d9b9fc1c3b2
DanSnow <dododavid006@gmail.com>
2016-04-18 02:39:40 +0800

    Update ExamNewView/BasicInfoTab

--- src/views/ExamNewView/BasicInfoTab.js
+++ src/views/ExamNewView/BasicInfoTab.js
@@ -1,7 +1,11 @@
 import React, { Component, PropTypes } from 'react';
 import { autobind } from 'core-decorators';
 
+import { Row, Col } from 'react-flexbox-grid';
 import TextField from 'material-ui/TextField';
+import DatePicker from 'material-ui/DatePicker';
+import TimePicker from 'material-ui/TimePicker';
+import Label from 'components/Label';
 
 export class BasicInfoTab extends Component {
   @autobind
@@ -18,10 +22,38 @@ export class BasicInfoTab extends Component {
   render() {
     return (
       <div>
-        <TextField
-          floatingLabelText='Name'
-          fullWidth
-          onChange={ this.handleNameChange } />
+        <div>
+          <TextField
+            floatingLabelText='Name'
+            fullWidth
+            onChange={ this.handleNameChange } />
+        </div>
+        <Row>
+          <Col md={ 2 } xs={ 12 }>
+            <Label>
+              Begin time:
+            </Label>
+          </Col>
+          <Col md={ 4 } xs={ 12 }>
+            <DatePicker hintText='Begin Date' />
+          </Col>
+          <Col md={ 4 } xs={ 12 }>
+            <TimePicker hintText='Begin Time' />
+          </Col>
+        </Row>
+        <Row>
+          <Col md={ 2 } xs={ 12 }>
+            <Label>
+              End time:
+            </Label>
+          </Col>
+          <Col md={ 4 } xs={ 12 }>
+            <DatePicker hintText='End Date' />
+          </Col>
+          <Col md={ 4 } xs={ 12 }>
+            <TimePicker hintText='End Time' />
+          </Col>
+        </Row>
       </div>
     );
   }
