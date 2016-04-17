ce32d6e8bdc5eddd444a6f4e863efff36fb2f398
DanSnow <dododavid006@gmail.com>
2016-04-17 21:59:05 +0800

    Remove csrf token

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,9 +2,7 @@ import rest from 'rest';
 import pathPrefix from 'rest/interceptor/pathPrefix';
 import mime from 'rest/interceptor/mime';
 import errorCode from 'rest/interceptor/errorCode';
-import csrf from 'rest/interceptor/csrf';
 import jwtAuth from './interceptors/jwt-auth';
-import Cookie from 'js-cookie';
 
 const api = rest
   .wrap(pathPrefix, { prefix: '/api/v1' })
@@ -13,10 +11,6 @@ const api = rest
     accept: 'application/json;q=0.8, text/plain;q=0.5, */*;q=0.2'
   })
   .wrap(jwtAuth)
-  .wrap(errorCode)
-  .wrap(csrf, {
-    name: 'X-XSRF-TOKEN',
-    token: Cookie.get('XSRF-TOKEN')
-  });
+  .wrap(errorCode);
 
 export default api;
