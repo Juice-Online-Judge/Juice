6ccc798b66d4412b8ee73df8ac1e02736a5004e5
DanSnow <dododavid006@gmail.com>
2016-04-18 16:20:45 +0800

    Add scroll-behavior

--- src/main.js
+++ src/main.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import { useRouterHistory } from 'react-router';
 import { createHistory } from 'history';
+import useScroll from 'scroll-behavior/lib/useStandardScroll';
 import routes from './routes';
 import Root from './containers/Root';
 import configureStore from './redux/configureStore';
 
 const historyConfig = { basename: __BASENAME__ };
-const browserHistory = useRouterHistory(createHistory)(historyConfig);
+const browserHistory = useScroll(useRouterHistory(createHistory))(historyConfig);
 
 const initialState = window.__INITIAL_STATE__;
 const { store, history } = configureStore({ initialState, browserHistory });
