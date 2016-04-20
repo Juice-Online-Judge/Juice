64d65952a8055df5b64a6773797a05c1d67703ce
DanSnow <dododavid006@gmail.com>
2016-04-20 16:03:37 +0800

    Add perf

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import useScroll from 'scroll-behavior/lib/useStandardScroll';
 import routes from './routes';
 import Root from './containers/Root';
 import configureStore from './redux/configureStore';
+import 'expose?Perf!react-addons-perf';
 
 const historyConfig = { basename: __BASENAME__ };
 const browserHistory = useScroll(useRouterHistory(createHistory))(historyConfig);
