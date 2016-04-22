60581105aee0f24452b0912358260f4ae4e93c84
BePsvPT <freedom@csie.io>
2016-04-22 09:40:58 +0800

    Fix incorrect validator

--- app/Validators/ExamQuestionInfoValidator.php
+++ app/Validators/ExamQuestionInfoValidator.php
@@ -17,22 +17,28 @@ class ExamQuestionInfoValidator
      */
     public function validate($attribute, $value, $parameters, Validator $validator)
     {
+        static $availableType = [null, 'proportion', 'portion'];
+
         $input = json_decode($value, true);
 
         if (! $input) {
             return false;
-        } elseif (! isset($input['score']) || ! is_float($input['score']) || ! ($input['score'] > 0.0 && $input['score'] <= 100.0)) {
+        } elseif (! isset($input['score']) || ! is_numeric($input['score']) || ! ($input['score'] > 0.0 && $input['score'] <= 100.0)) {
             return false;
-        } elseif (! array_key_exists('type', $input) || ! in_array($input['type'], [null, 'proportion', 'portion'])) {
+        } elseif (! array_key_exists('type', $input) || ! in_array($input['type'], $availableType)) {
             return false;
-        } elseif ((is_null($input['type']) || 'proportion' === $input['type']) && 2 !== count($input)) {
-            return false;
-        } elseif (! ('portion' === $input['type'] && isset($input['goal']) && isset($input['reward']))) {
-            return false;
-        } elseif (! is_int($input['goal']) || ! number_between($input['goal'], 0, 100, false)) {
-            return false;
-        } elseif (! is_int($input['reward']) || ! number_between($input['reward'], 0, 100, false)) {
-            return false;
+        } elseif (is_null($input['type']) || 'proportion' === $input['type']) {
+            if (2 !== count($input)) {
+                return false;
+            }
+        } elseif ('portion' === $input['type']) {
+            if (! isset($input['goal']) || ! isset($input['reward'])) {
+                return false;
+            } elseif (! is_int($input['goal']) || ! number_between($input['goal'], 0, 100, false)) {
+                return false;
+            } elseif (! is_int($input['reward']) || ! number_between($input['reward'], 0, 100, false)) {
+                return false;
+            }
         }
 
         return true;
