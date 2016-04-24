15545b81b01bca684e720fd04c8228b775370952
DanSnow <dododavid006@gmail.com>
2016-04-24 13:39:52 +0800

    Rewrite to stateless component

--- src/views/NotFoundView/NotFoundView.js
+++ src/views/NotFoundView/NotFoundView.js
@@ -1,16 +1,15 @@
 import React from 'react';
+import { setDisplayName } from 'recompose';
 import { Link } from 'react-router';
 
-export class NotFoundView extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>404 Not found</h1>
-        <hr />
-        <Link to='/'>Back To Index</Link>
-      </div>
-    );
-  }
-}
+export const NotFoundView = setDisplayName('NotFoundView')(
+  () => (
+    <div>
+      <h1>404 Not found</h1>
+      <hr />
+      <Link to='/'>Back To Index</Link>
+    </div>
+  )
+);
 
 export default NotFoundView;
