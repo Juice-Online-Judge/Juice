0aab2c080ad0921e51f3df0ca38d8623beb2eb41
DanSnow <dododavid006@gmail.com>
2016-04-24 11:46:30 +0800

    Fix style

--- src/redux/selectors/maxPageSelector.js
+++ src/redux/selectors/maxPageSelector.js
@@ -2,10 +2,9 @@ import { createSelector } from 'reselect';
 
 const getTotal = (state) => state.get('total');
 
-export const createMaxPageSelector = (perPage = 10) => {
-  return createSelector([getTotal], (total) => {
-    return Math.ceil(total / perPage);
-  });
-};
+export const createMaxPageSelector = (perPage = 10) => createSelector(
+  [getTotal],
+  (total) => Math.ceil(total / perPage)
+);
 
 export default createMaxPageSelector;
