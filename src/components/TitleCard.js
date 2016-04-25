09d96e1fe9914edbd6cc2299aea48e6acbcf37a1
DanSnow <dododavid006@gmail.com>
2016-04-25 16:51:59 +0800

    Rewrite as stateless component

--- src/components/TitleCard.js
+++ src/components/TitleCard.js
@@ -1,28 +1,28 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
+import setDisplayName from 'recompose/setDisplayName';
+import setPropTypes from 'recompose/setPropTypes';
+import compose from 'recompose/compose';
 
 import Card from 'material-ui/Card/Card';
 import CardTitle from 'material-ui/Card/CardTitle';
 
-class TitleCard extends Component {
-  render() {
-    return (
-      <Card>
-        <CardTitle
-          style={ this.props.style }
-          title={ this.props.title }
-          subtitle={ this.props.subtitle }>
-          { this.props.children }
-        </CardTitle>
-      </Card>
-    );
-  }
-
-  static propTypes = {
+const TitleCard = compose(
+  setPropTypes({
     style: PropTypes.object,
     children: PropTypes.node,
     title: PropTypes.string,
     subtitle: PropTypes.string
-  };
-}
+  }),
+  setDisplayName('TitleCard')
+)(({ style, title, subtitle, children }) => (
+  <Card>
+    <CardTitle
+      style={ this.props.style }
+      title={ this.props.title }
+      subtitle={ this.props.subtitle }>
+      { this.props.children }
+    </CardTitle>
+  </Card>
+));
 
 export default TitleCard;
