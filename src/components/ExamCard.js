97a61ff77d669e83c81882be87ca49ac08fa1260
DanSnow <dododavid006@gmail.com>
2016-04-25 16:31:23 +0800

    Rewrite as stateless component

--- src/components/ExamCard.js
+++ src/components/ExamCard.js
@@ -1,28 +1,27 @@
-import React, { Component, PropTypes } from 'react';
+import React, { PropTypes } from 'react';
 
 import { Link } from 'react-router';
+import setDisplayName from 'recompose/setDisplayName';
+import setPropTypes from 'recompose/setPropTypes';
+import compose from 'recompose/compose';
 
 import TitleCard from './TitleCard';
 import styles from 'lib/styles';
 
-class ExamCard extends Component {
-  render() {
-    const { id, name, beganTime, endedTime } = this.props;
-    return (
-      <Link to={ `/exams/${id}` } style={ styles.noUnderline }>
-        <TitleCard
-          title={ name }
-          subtitle={ `${beganTime} ~ ${endedTime}` } />
-      </Link>
-    );
-  }
-
-  static propTypes = {
+const ExamCard = compose(
+  setPropTypes({
     id: PropTypes.number.isRequired,
     name: PropTypes.string.isRequired,
     beganTime: PropTypes.string.isRequired,
     endedTime: PropTypes.string.isRequired
-  };
-}
+  }),
+  setDisplayName('ExamCard')
+)(({ id, name, beganTime, endedTime }) => (
+  <Link to={ `/exams/${id}` } style={ styles.noUnderline }>
+    <TitleCard
+      title={ name }
+      subtitle={ `${beganTime} ~ ${endedTime}` } />
+  </Link>
+));
 
 export default ExamCard;
