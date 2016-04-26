6c8ddaa63035fdcddf20ce4a9ab9fa9ac36b3060
DanSnow <dododavid006@gmail.com>
2016-04-26 11:12:32 +0800

    Add Message

---
+++ src/components/Message.js
@@ -0,0 +1,22 @@
+import React, { PropTypes } from 'react';
+import setDisplayName from 'recompose/setDisplayName';
+import setPropTypes from 'recompose/setPropTypes';
+import compose from 'recompose/compose';
+import Snackbar from 'material-ui/Snackbar';
+
+const Message = compose(
+  setPropTypes({
+    open: PropTypes.bool.isRequired,
+    message: PropTypes.string.isRequired,
+    onRequestClose: PropTypes.func.isRequired
+  }),
+  setDisplayName('Message')
+)(({ open, message, onRequestClose }) => (
+  <Snackbar
+    open={ open }
+    message={ message }
+    autoHideDuration={ 1000 }
+    onRequestClose={ onRequestClose } />
+));
+
+export default Message;
