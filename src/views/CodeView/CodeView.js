8641460c4d19e533ca69a3147cc3e5666ff3b5d6
DanSnow <dododavid006@gmail.com>
2016-04-28 17:21:31 +0800

    Add score field

--- src/views/CodeView/CodeView.js
+++ src/views/CodeView/CodeView.js
@@ -1,8 +1,12 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
+import { autobind } from 'core-decorators';
 
-import { fetchCode, fetchSubmission } from 'redux/modules/submission';
 import { Row, Col } from 'react-flexbox-grid';
+import { fetchCode, fetchSubmission, patchSubmissionCorrectness } from 'redux/modules/submission';
+import { createIsAdminSelector } from 'redux/modules/account';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
 import Inset from 'layouts/Inset';
 import CodePane from 'components/CodePane';
 import DownloadButton from 'components/DownloadButton';
@@ -10,18 +14,53 @@ import DownloadButton from 'components/DownloadButton';
 class CodeView extends Component {
   componentWillMount() {
     const { id } = this.props.params;
+    const { submission } = this.props;
     this.props.fetchSubmission(id);
     this.props.fetchCode(id);
+    this.setState({
+      correctness: submission.getIn(['entities', 'submission', `${id}`, 'judge', 'correctness'], 0)
+    });
+  }
+
+  @autobind
+  handleScoreChange({ target: { value } }) {
+    this.setState({ correctness: value });
+  }
+
+  @autobind
+  handleSetScore(event) {
+    const { id } = this.props.params;
+    this.props.patchSubmissionCorrectness(id, this.state.score);
   }
 
   render() {
-    const { id } = this.props.params;
-    const code = this.props.submission.get('code');
-    const lang = this.props.submission.getIn(['entities', 'submission', `${id}`, 'language']);
+    const { id, examId } = this.props.params;
+    const { admin, submission } = this.props;
+    const code = submission.get('code');
+    const lang = submission.getIn(['entities', 'submission', `${id}`, 'language']);
     const ext = lang === 'c++' ? 'cpp' : lang;
     return (
       <Inset>
-        <Row end='md'>
+        <Row middle='md' end='md'>
+          {
+            examId && admin ? (
+              <Col md={ 4 }>
+                <Row middle='md'>
+                  <Col md={ 6 }>
+                    <TextField
+                      onChange={ this.handleScoreChange }
+                      floatingLabelText='Score'
+                      value={ this.state.correctness } />
+                  </Col>
+                  <Col md={ 6 }>
+                    <FlatButton
+                      label='Set score'
+                      onTouchTap={ this.handleSetScore } />
+                  </Col>
+                </Row>
+              </Col>
+            ) : null
+          }
           <Col md={ 3 }>
             <DownloadButton label='Download Code' text={ code } filename={ `submission${id}.${ext}` } />
           </Col>
@@ -31,13 +70,20 @@ class CodeView extends Component {
     );
   }
 
+  state = {
+    correctness: null
+  };
+
   static propTypes = {
     params: PropTypes.object.isRequired,
+    admin: PropTypes.bool.isRequired,
     submission: PropTypes.object.isRequired,
+    patchSubmissionCorrectness: PropTypes.func.isRequired,
     fetchSubmission: PropTypes.func.isRequired,
     fetchCode: PropTypes.func.isRequired
   };
 }
 
-export default connect((state) => ({ submission: state.submission }),
-  { fetchCode, fetchSubmission })(CodeView);
+const isAdminSelector = createIsAdminSelector();
+export default connect((state) => ({ submission: state.submission, admin: isAdminSelector(state) }),
+  { fetchCode, fetchSubmission, patchSubmissionCorrectness })(CodeView);
