1fff12bcf6126342fa218b1913c00923a9f5be4b
DanSnow <dododavid006@gmail.com>
2016-05-03 14:37:09 +0800

    Change to class component; Calculate position

--- src/components/LoadingContainer.js
+++ src/components/LoadingContainer.js
@@ -1,27 +1,37 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import { connect } from 'react-redux';
-import setDisplayName from 'recompose/setDisplayName';
-import setPropTypes from 'recompose/setPropTypes';
-import compose from 'recompose/compose';
 
 import CenterLoading from './CenterLoading';
 import { isPendingSelector } from 'redux/modules/app';
 
-const LoadingContainer = compose(
-  connect((state) => ({ pending: isPendingSelector(state) })),
-  setPropTypes({
+class LoadingContainer extends Component {
+  componentDidMount() {
+    this.setState({
+      left: this.refs.container.offsetWidth / 2 - 20
+    });
+  }
+
+  render() {
+    const { pending, children } = this.props;
+    return (
+      <div ref='container' style={ styles.container }>
+        <CenterLoading left={ this.state.left } loading={ pending } />
+        { children }
+      </div>
+    );
+  }
+
+  state = {
+    left: window.innerWidth / 2 - 20
+  };
+
+  static propTypes = {
     children: PropTypes.node,
     pending: PropTypes.bool.isRequired
-  }),
-  setDisplayName('LoadingContainer')
-)(({ pending, children }) => (
-  <div style={ styles.container }>
-    <CenterLoading loading={ pending } />
-    { children }
-  </div>
-));
+  };
+}
 
-export default LoadingContainer;
+export default connect((state) => ({ pending: isPendingSelector(state) }))(LoadingContainer);
 
 const styles = {
   container: {
