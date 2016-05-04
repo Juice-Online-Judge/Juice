068f669fe82667f8f106a3704c335b7941c315f4
DanSnow <dododavid006@gmail.com>
2016-05-04 14:49:52 +0800

    Update MarkdownEditor
    1. Set min height for preview area
    2. Add style button

--- src/components/MarkdownEditor.js
+++ src/components/MarkdownEditor.js
@@ -1,18 +1,59 @@
 import React, { PropTypes, Component } from 'react';
+import { findDOMNode } from 'react-dom';
 import { autobind } from 'core-decorators';
+import qwery from 'qwery';
 
 import Toolbar from 'material-ui/Toolbar/Toolbar';
 import ToolbarGroup from 'material-ui/Toolbar/ToolbarGroup';
 import ToolbarTitle from 'material-ui/Toolbar/ToolbarTitle';
 import FlatButton from 'material-ui/FlatButton';
+import IconButton from 'material-ui/IconButton';
 import TextField from 'material-ui/TextField';
 import Markdown from 'react-markdown';
 import PreviewIcon from 'material-ui/svg-icons/action/toc';
+import BoldIcon from 'material-ui/svg-icons/editor/format-bold';
+import ItalicIcon from 'material-ui/svg-icons/editor/format-italic';
 
 class MarkdownEditor extends Component {
   @autobind
   handlePreviewToggle() {
     this.setState({ preview: !this.state.preview });
+  }
+
+  @autobind
+  getTextArea(textField) {
+    if (!textField) {
+      return;
+    }
+    this.textarea = qwery('textarea:nth-child(2)', findDOMNode(textField))[0];
+  }
+
+  @autobind
+  handleBold() {
+    this.appendTextAndMoveCursor('****');
+  }
+
+  @autobind
+  handleItalic() {
+    this.appendTextAndMoveCursor('__');
+  }
+
+  appendTextAndMoveCursor(appendText) {
+    const { text } = this.state;
+    this.setState({ text: `${text}${appendText}` }, () => {
+      this.moveCursorBack(appendText.length / 2);
+    });
+  }
+
+  moveCursorBack(count) {
+    const { preview, text } = this.state;
+    const pos = text.length - count;
+    if (preview) {
+      return;
+    }
+
+    this.textarea.setSelectionRange(pos, pos);
+    this.textarea.focus();
   }
 
   @autobind
@@ -44,13 +85,26 @@ class MarkdownEditor extends Component {
             <ToolbarTitle>
               Markdown Editor
             </ToolbarTitle>
+            <IconButton
+              disabled={ preview }
+              onTouchTap={ this.handleBold }>
+              <BoldIcon />
+            </IconButton>
+            <IconButton
+              disabled={ preview }
+              onTouchTap={ this.handleItalic }>
+              <ItalicIcon />
+            </IconButton>
           </ToolbarGroup>
         </Toolbar>
         {
           preview ? (
-            <Markdown source={ text || 'Nothing to preview' } />
+            <div style={ { height: '15em' } }>
+              <Markdown source={ text || 'Nothing to preview' } />
+            </div>
           ) : (
             <TextField
+              ref={ this.getTextArea }
               onChange={ this.handleChange }
               value={ text }
               multiLine
