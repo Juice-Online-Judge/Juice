aab26c2ab036f4d1e96d140484560d4f298fd79d
DanSnow <dododavid006@gmail.com>
2016-05-05 20:38:14 +0800

    Add test

---
+++ tests/layouts/Inset.spec.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { mount, render, shallow } from 'enzyme';
+import Inset from 'layouts/Inset';
+import { Grid, Row, Col } from 'react-flexbox-grid';
+
+describe('<Inset />', () => {
+  it('render <Grid /> component', () => {
+    const wrapper = shallow(<Inset />);
+    expect(wrapper.find(Grid)).to.have.length(1);
+  });
+
+  it('render <Row /> component', () => {
+    const wrapper = shallow(<Inset />);
+    expect(wrapper.find(Row)).to.have.length(1);
+  });
+
+  it('render <Col /> component', () => {
+    const wrapper = shallow(<Inset />);
+    expect(wrapper.find(Col)).to.have.length(1);
+  });
+});
