019b5978e0ec625c0a4fcf5621061405fa813412
DanSnow <dododavid006@gmail.com>
2016-05-05 21:04:43 +0800

    Fix deprecation

--- tests/layouts/Inset.spec.js
+++ tests/layouts/Inset.spec.js
@@ -6,16 +6,16 @@ import { Grid, Row, Col } from 'react-flexbox-grid';
 describe('<Inset />', () => {
   it('render <Grid /> component', () => {
     const wrapper = shallow(<Inset />);
-    expect(wrapper.find(Grid)).to.have.length(1);
+    expect(wrapper.find(Grid)).to.have.lengthOf(1);
   });
 
   it('render <Row /> component', () => {
     const wrapper = shallow(<Inset />);
-    expect(wrapper.find(Row)).to.have.length(1);
+    expect(wrapper.find(Row)).to.have.lengthOf(1);
   });
 
   it('render <Col /> component', () => {
     const wrapper = shallow(<Inset />);
-    expect(wrapper.find(Col)).to.have.length(1);
+    expect(wrapper.find(Col)).to.have.lengthOf(1);
   });
 });
