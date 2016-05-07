c7937cd95d2e38e884ddcf285835cd6a3eec4341
DanSnow <dododavid006@gmail.com>
2016-05-07 16:58:20 +0800

    Fix bug; Add prop loading test

--- tests/components/CenterLoading.spec.js
+++ tests/components/CenterLoading.spec.js
@@ -6,18 +6,34 @@ import RefreshIndicator from 'material-ui/RefreshIndicator';
 
 describe('(Component) <CenterLoading />', () => {
   it('Render <Row /> with prop center', () => {
-    const wrapper = shallow(<CenterLoading status='hide' left={ 10 } />);
+    const wrapper = shallow(<CenterLoading loading={ false } left={ 10 } />);
     expect(wrapper).to.have.exactly(1).descendants(Row);
     expect(wrapper.find(Row).first()).to.have.prop('center', 'xs');
   });
 
   it('Render <Col />', () => {
-    const wrapper = shallow(<CenterLoading status='hide' left={ 10 } />);
+    const wrapper = shallow(<CenterLoading loading={ false } left={ 10 } />);
     expect(wrapper).to.have.exactly(1).descendants(Col);
   });
 
   it('Render <RefreshIndicator />', () => {
-    const wrapper = shallow(<CenterLoading status='hide' left={ 10 } />);
+    const wrapper = shallow(<CenterLoading loading={ false } left={ 10 } />);
     expect(wrapper).to.have.exactly(1).descendants(RefreshIndicator);
+  });
+
+  describe('(Prop) loading', () => {
+    context('When false', () => {
+      it('Render <RefreshIndicator status="hide" />', () => {
+        const wrapper = shallow(<CenterLoading loading={ false } left={ 10 } />);
+        expect(wrapper.find(RefreshIndicator)).to.have.prop('status', 'hide');
+      });
+    });
+
+    context('When true', () => {
+      it('Render <RefreshIndicator status="loading" />', () => {
+        const wrapper = shallow(<CenterLoading loading left={ 10 } />);
+        expect(wrapper.find(RefreshIndicator)).to.have.prop('status', 'loading');
+      });
+    })
   });
 });
