1d15bbfa7741129036397abaea8295d80d8c1098
DanSnow <dododavid006@gmail.com>
2016-05-07 14:35:17 +0800

    Fix style

--- tests/framework.spec.js
+++ tests/framework.spec.js
@@ -1,6 +1,6 @@
-import assert from 'assert'
-import React from 'react'
-import {mount, render, shallow} from 'enzyme'
+import assert from 'assert';
+import React from 'react';
+import { mount, render, shallow } from 'enzyme';
 
 class Fixture extends React.Component {
   render () {
@@ -9,37 +9,37 @@ class Fixture extends React.Component {
         <input id='checked' defaultChecked />
         <input id='not' defaultChecked={false} />
       </div>
-    )
+    );
   }
 }
 
-describe('(Framework) Karma Plugins', function () {
-  it('Expose "expect" globally.', function () {
-    assert.ok(expect)
-  })
+describe('(Framework) Karma Plugins', () => {
+  it('Expose "expect" globally.', () => {
+    assert.ok(expect);
+  });
 
-  it('Expose "should" globally.', function () {
-    assert.ok(should)
-  })
+  it('Expose "should" globally.', () => {
+    assert.ok(should);
+  });
 
-  it('Have chai-as-promised helpers.', function () {
-    const pass = new Promise(res => res('test'))
-    const fail = new Promise((res, rej) => rej())
+  it('Have chai-as-promised helpers.', () => {
+    const pass = new Promise(res => res('test'));
+    const fail = new Promise((res, rej) => rej());
 
     return Promise.all([
       expect(pass).to.be.fulfilled,
       expect(fail).to.not.be.fulfilled
-    ])
-  })
+    ]);
+  });
 
-  it('Have chai-enzyme working', function() {
-    let wrapper = shallow(<Fixture />)
-    expect(wrapper.find('#checked')).to.be.checked()
+  it('Have chai-enzyme working', () => {
+    let wrapper = shallow(<Fixture />);
+    expect(wrapper.find('#checked')).to.be.checked();
 
-    wrapper = mount(<Fixture />)
-    expect(wrapper.find('#checked')).to.be.checked()
+    wrapper = mount(<Fixture />);
+    expect(wrapper.find('#checked')).to.be.checked();
 
-    wrapper = render(<Fixture />)
-    expect(wrapper.find('#checked')).to.be.checked()
-  })
-})
+    wrapper = render(<Fixture />);
+    expect(wrapper.find('#checked')).to.be.checked();
+  });
+});
