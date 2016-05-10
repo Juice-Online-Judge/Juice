26268943e5a8cc014fd1219ada9d6f2aa51e102e
DanSnow <dododavid006@gmail.com>
2016-05-10 11:52:39 +0800

    Remove development override

--- config/environments.js
+++ config/environments.js
@@ -1,9 +1,6 @@
 /* eslint key-spacing:0 */
 
 export default {
-  development: (config) => ({
-    compiler_public_path: `http://${config.server_host}:${config.server_port}/`
-  }),
   production: (config) => ({
     compiler_output_name     : '[name].[hash].js',
     compiler_fail_on_warning : false,
