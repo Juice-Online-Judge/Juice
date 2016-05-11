59d2e7be6312929c20584e9eeac8413f47ce8ae3
DanSnow <dododavid006@gmail.com>
2016-05-12 01:47:46 +0800

    Remove doOnReceiveProps

--- src/lib/redirectComponent.js
+++ src/lib/redirectComponent.js
@@ -1,37 +1,58 @@
+import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
 import setDisplayName from 'recompose/setDisplayName';
 import wrapDisplayName from 'recompose/wrapDisplayName';
-import doOnReceiveProps from 'recompose/doOnReceiveProps';
+import createElement from 'recompose/createElement';
 import compose from 'recompose/compose';
 import omitProps from './omitProps';
 
 import { RequestStatus } from 'lib/const';
-import { actions as appActions } from 'redux/modules/app';
+import { clearError } from 'redux/modules/app';
 
 export const redirectConnect = connect((state) => ({ app: state.app }),
-  Object.assign({}, appActions, { replace }));
+  { replace, clearError });
 
 export const redirectComponent = (name, shouldRedirectPath, WrappedComponent) => {
+  const redirectComponentHoc = (WrappedComponent) => {
+    return class extends Component {
+      componentWillMount() {
+        this.checkRedirect(this.props);
+      }
+
+      componentWillReceiveProps(nextProps) {
+        this.checkRedirect(nextProps);
+      }
+
+      checkRedirect(props) {
+        const { app, clearError, replace } = props;
+        if (app.get('status') === RequestStatus.FAIL) {
+          const errorCode = app.getIn(['error', 'code']);
+          const redirectPath = shouldRedirectPath(errorCode);
+          clearError();
+          if (redirectPath) {
+            replace(redirectPath);
+          }
+        }
+      }
+
+      render() {
+        return createElement(WrappedComponent, this.props);
+      }
+
+      static propTypes = {
+        app: PropTypes.object.isRequired,
+        clearError: PropTypes.func.isRequired,
+        replace: PropTypes.func.isRequired
+      };
+    };
+  };
   return compose(
     redirectConnect,
     setDisplayName(wrapDisplayName(WrappedComponent, name)),
-    doOnReceiveProps((props) => {
-      props.clearError();
-      if (props.app.get('status') === RequestStatus.FAIL) {
-        const errorCode = props.app.getIn(['error', 'code']);
-        const redirectPath = shouldRedirectPath(errorCode);
-        props.clearError();
-        if (redirectPath) {
-          this.props.replace(redirectPath);
-        }
-      }
-    }),
+    redirectComponentHoc,
     omitProps([
       'app',
-      'setStatus',
-      'clearStatus',
-      'setError',
       'clearError'
     ])
   )(WrappedComponent);
--- src/lib/redirectNotAdmin.js
+++ src/lib/redirectNotAdmin.js
@@ -1,25 +1,52 @@
+import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
 import setDisplayName from 'recompose/setDisplayName';
 import wrapDisplayName from 'recompose/wrapDisplayName';
-import doOnReceiveProps from 'recompose/doOnReceiveProps';
+import createElement from 'recompose/createElement';
 import omitProps from './omitProps';
 import compose from 'recompose/compose';
 import { createIsAdminSelector, isValidSelector } from 'redux/modules/account';
 
 const redirectNotAdmin = (WrappedComponent) => {
   const isAdminSelector = createIsAdminSelector();
+
+  const redirectNotAdminHoc = (WrappedComponent) => {
+    return class extends Component {
+      componentWillMount() {
+        this.checkRedirect(this.props);
+      }
+
+      componentWillReceiveProps(nextProps) {
+        this.checkRedirect(nextProps);
+      }
+
+      checkRedirect(props) {
+        const { valid, admin, replace } = props;
+        if (valid && !admin) {
+          replace('/permission-denied');
+        }
+      }
+
+      render() {
+        return createElement(WrappedComponent, this.props);
+      }
+
+      static propTypes = {
+        valid: PropTypes.bool.isRequired,
+        admin: PropTypes.bool.isRequired,
+        replace: PropTypes.func.isRequired
+      };
+    };
+  };
+
   const EnhancedComponent = compose(
     connect((state) => ({
       admin: isAdminSelector(state),
       valid: isValidSelector(state)
     }), { replace }),
     setDisplayName(wrapDisplayName(WrappedComponent, 'redirectNotAdmin')),
-    doOnReceiveProps(({ admin, valid, replace }) => {
-      if (valid && !admin) {
-        replace('/permission-denied');
-      }
-    }),
+    redirectNotAdminHoc,
     omitProps(['admin', 'valid', 'replace'])
   )(WrappedComponent);
   return EnhancedComponent;
