45e29339cf414d41a1c031162cb040fb8b27483c
DanSnow <dododavid006@gmail.com>
2016-05-17 15:15:07 +0800

    Use lifecycle

--- src/lib/redirectNotAdmin.js
+++ src/lib/redirectNotAdmin.js
@@ -1,49 +1,33 @@
-import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
 import setDisplayName from 'recompose/setDisplayName';
 import wrapDisplayName from 'recompose/wrapDisplayName';
-import createElement from 'recompose/createElement';
+import lifecycle from 'recompose/lifecycle';
 import omitProps from './omitProps';
 import compose from 'recompose/compose';
-import { createIsAdminSelector, isValidSelector } from 'redux/modules/account';
+import { isNotAdminSelector } from 'redux/modules/account';
 
 const redirectNotAdmin = (WrappedComponent) => {
-  const isAdminSelector = createIsAdminSelector();
-
-  const redirectNotAdminHoc = (WrappedComponent) => {
-    return class extends Component {
-      componentWillMount() {
-        this.checkRedirect(this.props);
+  const redirectNotAdminHoc = lifecycle({
+    componentWillMount() {
+      this.checkRedirect(this.props);
+    },
+    componentWillReceiveProps(nextProps) {
+      this.checkRedirect(nextProps);
+    },
+    checkRedirect(props) {
+      const { isNotAdmin, replace } = props;
+      console.log('IsNotAdmin', isNotAdmin);
+      if (isNotAdmin) {
+        console.log('Do replace');
+        replace('/permission-denied');
       }
-
-      componentWillReceiveProps(nextProps) {
-        this.checkRedirect(nextProps);
-      }
-
-      checkRedirect(props) {
-        const { valid, admin, replace } = props;
-        if (valid && !admin) {
-          replace('/permission-denied');
-        }
-      }
-
-      render() {
-        return createElement(WrappedComponent, this.props);
-      }
-
-      static propTypes = {
-        valid: PropTypes.bool.isRequired,
-        admin: PropTypes.bool.isRequired,
-        replace: PropTypes.func.isRequired
-      };
-    };
-  };
+    }
+  });
 
   const EnhancedComponent = compose(
     connect((state) => ({
-      admin: isAdminSelector(state),
-      valid: isValidSelector(state)
+      isNotAdmin: isNotAdminSelector(state)
     }), { replace }),
     setDisplayName(wrapDisplayName(WrappedComponent, 'redirectNotAdmin')),
     redirectNotAdminHoc,
