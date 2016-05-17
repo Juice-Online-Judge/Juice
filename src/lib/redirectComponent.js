69f9ad47fcc0917dd78e70a9986fa5afcbbdad52
DanSnow <dododavid006@gmail.com>
2016-05-17 15:17:45 +0800

    Use lifecycle; Refactor

--- src/lib/redirectComponent.js
+++ src/lib/redirectComponent.js
@@ -1,9 +1,8 @@
-import { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { replace } from 'react-router-redux';
 import setDisplayName from 'recompose/setDisplayName';
 import wrapDisplayName from 'recompose/wrapDisplayName';
-import createElement from 'recompose/createElement';
+import lifecycle from 'recompose/lifecycle';
 import compose from 'recompose/compose';
 import omitProps from './omitProps';
 
@@ -14,39 +13,30 @@ export const redirectConnect = connect((state) => ({ app: state.app }),
   { replace, clearError });
 
 export const redirectComponent = (name, shouldRedirectPath, WrappedComponent) => {
-  const redirectComponentHoc = (WrappedComponent) => {
-    return class extends Component {
-      componentWillMount() {
-        this.checkRedirect(this.props);
+  const redirectComponentHoc = lifecycle({
+    componentWillMount() {
+      this.checkRedirect(this.props);
+    },
+    componentWillReceiveProps(nextProps) {
+      this.checkRedirect(nextProps);
+    },
+    checkRedirect(props) {
+      const { app, clearError, replace } = props;
+
+      if (app.get('status') !== RequestStatus.FAIL) {
+        return;
       }
 
-      componentWillReceiveProps(nextProps) {
-        this.checkRedirect(nextProps);
-      }
+      const errorCode = app.getIn(['error', 'code']);
+      const redirectPath = shouldRedirectPath(errorCode);
 
-      checkRedirect(props) {
-        const { app, clearError, replace } = props;
-        if (app.get('status') === RequestStatus.FAIL) {
-          const errorCode = app.getIn(['error', 'code']);
-          const redirectPath = shouldRedirectPath(errorCode);
-          clearError();
-          if (redirectPath) {
-            replace(redirectPath);
-          }
-        }
-      }
+      clearError();
 
-      render() {
-        return createElement(WrappedComponent, this.props);
+      if (redirectPath) {
+        replace(redirectPath);
       }
-
-      static propTypes = {
-        app: PropTypes.object.isRequired,
-        clearError: PropTypes.func.isRequired,
-        replace: PropTypes.func.isRequired
-      };
-    };
-  };
+    }
+  });
 
   return compose(
     redirectConnect,
