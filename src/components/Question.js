18937d56467b61f264a9bc0cb3b199299de550c3
DanSnow <dododavid006@gmail.com>
2016-05-17 22:37:52 +0800

    Use Markdown

--- src/components/Question.js
+++ src/components/Question.js
@@ -8,7 +8,7 @@ import Card from 'material-ui/Card/Card';
 import CardTitle from 'material-ui/Card/CardTitle';
 import CardText from 'material-ui/Card/CardText';
 import CardActions from 'material-ui/Card/CardActions';
-import Markdown from 'react-markdown';
+import Markdown from './Markdown';
 
 import SubmitCode from './SubmitCode';
 
