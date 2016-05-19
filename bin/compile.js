15f8d5c9901cdf168526b9cb91a421ec03c72d68
DanSnow <dododavid006@gmail.com>
2016-05-19 20:24:46 +0800

    Remove copy of static file

--- bin/compile.js
+++ bin/compile.js
@@ -1,11 +1,9 @@
-import fs from 'fs-extra';
 import _debug from 'debug';
 import webpackCompiler from '../build/webpack-compiler';
 import webpackConfig from '../build/webpack.config';
 import config from '../config';
 
 const debug = _debug('app:bin:compile');
-const paths = config.utils_paths;
 
 ;(async function () {
   try {
@@ -15,8 +13,6 @@ const paths = config.utils_paths;
       debug('Config set to fail on warning, exiting with status code "1".');
       process.exit(1);
     }
-    debug('Copy static assets to dist folder.');
-    fs.copySync(paths.client('static'), paths.dist());
   } catch (e) {
     debug('Compiler encountered an error.', e);
     process.exit(1);
