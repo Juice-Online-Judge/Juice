4cb2a13d618b867c5a2cca4894a6bf4991ccd1f6
DanSnow <dododavid006@gmail.com>
2016-05-19 18:28:08 +0800

    Add #redirectOnLogin

---
+++ src/lib/redirectOnLogin.js
@@ -0,0 +1,13 @@
+import redirectComponent from './redirectComponent';
+import { isLoginSelector } from 'redux/modules/account';
+
+export const redirectOnLogin = redirectComponent(
+  'RedirectOnLogin',
+  (state) => ({ isLogin: isLoginSelector(state) }),
+  ({ isLogin }) => (isLogin ? '/' : null),
+  {
+    omitProps: 'isLogin'
+  }
+);
+
+export default redirectOnLogin;
