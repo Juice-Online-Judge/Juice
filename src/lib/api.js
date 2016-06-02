a2baa01bf4a309e544aed0cbf913d98dbf186fab
DanSnow <dododavid006@gmail.com>
2016-06-02 22:52:20 +0800

    Rename xhr-header -> headers

--- src/lib/api.js
+++ src/lib/api.js
@@ -3,7 +3,7 @@ import pathPrefix from 'rest/interceptor/pathPrefix';
 import mime from 'rest/interceptor/mime';
 import errorCode from 'rest/interceptor/errorCode';
 import jwtAuth from './interceptors/jwt-auth';
-import xhrHeader from './interceptors/xhr-header';
+import headers from './interceptors/headers';
 
 const api = rest
   .wrap(pathPrefix, { prefix: '/api/v1' })
@@ -12,7 +12,7 @@ const api = rest
     accept: 'application/json;q=0.8, text/plain;q=0.5, */*;q=0.2'
   })
   .wrap(jwtAuth)
-  .wrap(xhrHeader)
+  .wrap(headers)
   .wrap(errorCode);
 
 export default api;
