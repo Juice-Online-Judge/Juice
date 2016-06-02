746e03e04c48a144b8b821ee68b2e3ccf01239f1
DanSnow <dododavid006@gmail.com>
2016-06-02 22:53:07 +0800

    Add header

--- src/lib/interceptors/headers.js
+++ src/lib/interceptors/headers.js
@@ -4,6 +4,7 @@ const xhrHeader = interceptor({
   request(request) {
     request.headers = request.headers || {};
     request.headers['X-Requested-With'] = 'XMLHttpRequest';
+    request.headers['Accept'] = API_HEADER;
     return request;
   }
 });
