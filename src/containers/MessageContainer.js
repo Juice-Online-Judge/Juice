e1bfb8ee77ceccf60fa8ad4853b5025d5672e4d5
DanSnow <dododavid006@gmail.com>
2016-06-02 17:19:48 +0800

    Add MessageContainer

---
+++ src/containers/MessageContainer.js
@@ -0,0 +1,37 @@
+import React, { PropTypes, Component } from 'react';
+import { connect } from 'react-redux';
+import { bind } from 'decko';
+import Message from 'components/Message';
+import { setOpen } from 'redux/modules/message';
+
+export class MessageContainer extends Component {
+  @bind
+  handleClose() {
+    this.props.setOpen(false);
+  }
+
+  render() {
+    const { children, messageStore, message } = this.props;
+    return (
+      <div>
+        { children }
+        <Message
+          open={ messageStore.get('open') }
+          message={ message || messageStore.get('message') }
+          onRequestClose={ this.handleClose } />
+      </div>
+    );
+  }
+
+  static propTypes = {
+    message: PropTypes.string,
+    messageStore: PropTypes.object.isRequired,
+    setOpen: PropTypes.func.isRequired,
+    children: PropTypes.node
+  };
+}
+
+export default connect(
+  (state) => ({ messageStore: state.message }),
+  { setOpen }
+)(MessageContainer);
