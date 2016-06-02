9a7542e7a74a1bafef9c10d5fdeb1680bfb4cb1a
DanSnow <dododavid006@gmail.com>
2016-06-02 23:53:09 +0800

    Port to saga

--- src/redux/modules/message.js
+++ src/redux/modules/message.js
@@ -1,5 +1,7 @@
 import { createAction, handleActions } from 'redux-actions';
 import { Record } from 'immutable';
+import { takeEvery } from 'redux-saga';
+import { put } from 'redux-saga/effects';
 
 export const MessageState = new Record({
   open: false,
@@ -10,14 +12,20 @@ export const initialState = new MessageState();
 
 export const SET_MESSAGE = 'SET_MESSAGE';
 export const SET_OPEN = 'SET_OPEN';
+export const SHOW_MESSAGE = 'SHOW_MESSAGE';
 
 export const setMessage = createAction(SET_MESSAGE);
 export const setOpen = createAction(SET_OPEN);
+export const showMessage = createAction(SHOW_MESSAGE);
 
-export const showMessage = (msg) => (dispatch) => {
-  dispatch(setMessage(msg));
-  dispatch(setOpen(true));
-};
+export function* doShowMessage({ payload }) {
+  yield put(setMessage(payload));
+  yield put(setOpen(true));
+}
+
+export function* watcher() {
+  yield* takeEvery(SHOW_MESSAGE, doShowMessage);
+}
 
 export default handleActions({
   [SET_MESSAGE]: (state, { payload }) => state.set('message', payload),
