8546df4a7da2271ce7cacf3e7379546c14119b13
DanSnow <dododavid006@gmail.com>
2016-06-03 12:04:44 +0800

    Store validation info

--- src/lib/validateForm.js
+++ src/lib/validateForm.js
@@ -5,7 +5,13 @@ import setDisplayName from 'recompose/setDisplayName';
 import wrapDisplayName from 'recompose/wrapDisplayName';
 import getDisplayName from 'recompose/getDisplayName';
 import compose from 'recompose/compose';
-import { createGetComponentMessage, validateForm, clearValidationMessage } from 'redux/modules/validate';
+import {
+  createGetComponentMessage,
+  validateForm,
+  setValidationName,
+  setValidationRule,
+  clearValidationMessage
+} from 'redux/modules/validate';
 
 const validateFormHoc = (validateRule) => (WrappedComponent) => {
   const componentName = getDisplayName(WrappedComponent);
@@ -13,15 +19,26 @@ const validateFormHoc = (validateRule) => (WrappedComponent) => {
   const mapStates = (state) => getComponentMessage(state);
 
   return compose(
-    connect(mapStates, { validateForm, clearValidationMessage }),
+    connect(mapStates, {
+      validateForm,
+      clearValidationMessage,
+      setValidationName,
+      setValidationRule
+    }),
     lifecycle({
       componentWillMount() {
         this.props.clearValidationMessage(componentName);
+        this.props.setValidationName(componentName);
+        this.props.setValidationRule(validateRule);
+      },
+      componentWillUnmount() {
+        this.props.setValidationName(null);
+        this.props.setValidationRule({});
       }
     }),
     withHandlers({
       validateForm: (props) => (fields, cb) =>
-        props.validateForm(componentName, fields, validateRule, cb)
+        props.validateForm(fields, cb)
     }),
     setDisplayName(wrapDisplayName(WrappedComponent, 'Validate'))
   )(WrappedComponent);
--- src/redux/modules/validate.js
+++ src/redux/modules/validate.js
@@ -3,12 +3,18 @@ import { fromJS, Map } from 'immutable';
 import validate from 'validate.js';
 
 const initialState = fromJS({
+  name: null,
+  rule: {},
   message: {}
 });
 
+export const SET_VALIDATION_NAME = 'SET_VALIDATION_NAME';
 export const SET_VALIDATION_MESSAGE = 'SET_VALIDATION_MESSAGE';
+export const SET_VALIDATION_RULE = 'SET_VALIDATION_RULE';
 export const CLEAR_VALIDATION_MESSAGE = 'CLEAR_VALIDATION_MESSAGE';
 
+export const setValidationName = createAction(SET_VALIDATION_NAME);
+export const setValidationRule = createAction(SET_VALIDATION_RULE);
 export const setValidationMessage = createAction(SET_VALIDATION_MESSAGE, (name, error) => {
   const payload = {};
   payload[name] = error;
@@ -16,23 +22,18 @@ export const setValidationMessage = createAction(SET_VALIDATION_MESSAGE, (name, 
 });
 export const clearValidationMessage = createAction(CLEAR_VALIDATION_MESSAGE);
 
-export const validateForm = (name, fields, rule, cb) => (dispatch) => validate
-  .async(fields, rule)
-  .then(() => {
-    dispatch(clearValidationMessage(name));
-    cb(true);
-    return true;
-  })
-  .catch((error) => {
-    if (error instanceof Error) {
-      console.warn(error);
-      throw error;
-    } else {
-      dispatch(setValidationMessage(name, error));
-    }
-    cb(false);
-    return false;
-  });
+export const validateForm = (fields, cb) => (dispatch, getState) => {
+  const { validation } = getState();
+  const messages = validate(fields, validation.get('rule'));
+  const result = !messages;
+  if (result) {
+    dispatch(clearValidationMessage(validation.get('name')));
+  } else {
+    dispatch(setValidationMessage(validation.get('name'), messages));
+  }
+  cb(result);
+  return result;
+};
 
 export const createGetComponentMessage = (name) => (state) => ({
   validation: state.validate.getIn(['message', name], new Map())
@@ -45,6 +46,8 @@ export const actions = {
 };
 
 export default handleActions({
+  [SET_VALIDATION_NAME]: (state, { payload }) => state.set('name', payload),
+  [SET_VALIDATION_RULE]: (state, { payload }) => state.set('rule', fromJS(payload)),
   [SET_VALIDATION_MESSAGE]: (state, { payload }) => state.mergeIn(['message'], payload),
   [CLEAR_VALIDATION_MESSAGE]: (state, { payload }) => state.setIn(['message', payload], new Map())
 }, initialState);
