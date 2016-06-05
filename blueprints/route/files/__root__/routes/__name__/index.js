83658446a6273f4cc312942aa1fd8eeeb98aa4c2
DanSnow <dododavid006@gmail.com>
2016-06-06 00:22:55 +0800

    Add route blueprints

---
+++ blueprints/route/files/__root__/routes/__name__/index.js
@@ -0,0 +1,6 @@
+import <%= pascalEntityName %>View from 'views/<%= pascalEntityName %>View';
+
+export default {
+  path: '<%= camelEntityName %>',
+  component: <%= pascalEntityName %>View
+};
---
+++ blueprints/route/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  description() {
+    return 'generate routes';
+  },
+  fileMapTokens() {
+    return {
+      __name__(options) {
+        return options.entity.name;
+      }
+    };
+  }
+};
