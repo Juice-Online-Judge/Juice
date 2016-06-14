7388f88b0464f6aa6d184b02ae788e9791eaba3f
DanSnow <dododavid006@gmail.com>
2016-06-14 14:08:00 +0800

    refactor(redux): Port to saga

--- src/redux/modules/role.js
+++ src/redux/modules/role.js
@@ -1,3 +1,5 @@
+import { takeEvery } from 'redux-saga';
+import { put, call } from 'redux-saga/effects';
 import { createAction, handleActions } from 'redux-actions';
 import { Record, List, Map } from 'immutable';
 import { normalize, arrayOf } from 'normalizr';
@@ -14,17 +16,25 @@ const initialState = new RoleState();
 
 export const SET_ROLE = 'SET_ROLE';
 export const CLEAR_ROLE = 'CLEAR_ROLE';
+export const FETCH_ROLE = 'FETCH_ROLE';
 
 export const setRole = createAction(SET_ROLE, (data) => normalize(data, arrayOf(roleSchema)));
 export const clearRole = createAction(CLEAR_ROLE);
+export const fetchRole = createAction(FETCH_ROLE);
 
-export const fetchRole = () => (dispatch) => {
-  dispatch(request({
+export function* fetchRoleFlow() {
+  const { error, entity } = yield call(request, {
     path: 'roles'
-  }, (entity) => {
-    dispatch(setRole(entity));
-  }));
+  });
+
+  if (!error) {
+    yield put(setRole(entity));
+  }
 };
+
+export function* watcher() {
+  yield* takeEvery(FETCH_ROLE, fetchRoleFlow);
+}
 
 export const actions = {
   setRole,
