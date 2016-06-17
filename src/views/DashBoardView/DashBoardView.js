ea5ad8294ffa7ab6f2631a98aa095f2019d4fb3f
DanSnow <dododavid006@gmail.com>
2016-06-17 12:46:39 +0800

    fix(DashBoard): Remove test code

--- src/views/DashBoardView/DashBoardView.js
+++ src/views/DashBoardView/DashBoardView.js
@@ -1,36 +1,10 @@
 import React, { Component } from 'react'
-import { bind } from 'decko'
-import without from 'lodash/without'
-import concat from 'lodash/concat'
-
-import FlatButton from 'material-ui/FlatButton'
-import Toasts from 'components/Toasts'
 
 export class DashBoard extends Component {
-  @bind
-  handleRequestClose(id) {
-    this.setState({ messages: without(this.state.messages, id) })
-  }
-
-  @bind
-  addToast() {
-    this.setState({ messages: concat(this.state.messages, `Toast${this.count}`) })
-    this.count += 1
-  }
-
   render() {
     return (
-      <div>
-        <FlatButton label='Toast' onTouchTap={ this.addToast } />
-        <Toasts messages={ this.state.messages } onRequestClose={ this.handleRequestClose } />
-      </div>
+      <div />
     )
-  }
-
-  count = 0
-
-  state = {
-    messages: []
   }
 }
 
