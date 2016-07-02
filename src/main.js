4127ca59dd279d5a4c9d34ed2d590bdf4af75aaa
DanSnow <dododavid006@gmail.com>
2016-07-02 15:45:45 +0800

    refactor(main): Configure history and store; Render Root

--- src/main.js
+++ src/main.js
@@ -3,17 +3,20 @@ import './bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import injectTapEventPlugin from 'react-tap-event-plugin'
-import { useRouterHistory } from 'react-router'
+import { Provider } from 'react-redux'
+import useScroll from 'react-router-scroll'
+import { syncHistoryWithStore } from 'react-router-redux'
+import { Router, applyRouterMiddleware, useRouterHistory } from 'react-router'
 import { createHistory } from 'history'
 import routes from './routes'
-import Root from './containers/Root'
 import configureStore from './redux/configureStore'
 
 const historyConfig = { basename: __BASENAME__ }
 const browserHistory = useRouterHistory(createHistory)(historyConfig)
 
 const initialState = window.__INITIAL_STATE__
-const { store, history } = configureStore({ initialState, browserHistory })
+const store = configureStore({ initialState, browserHistory })
+const history = syncHistoryWithStore(browserHistory, store)
 
 injectTapEventPlugin()
 
@@ -21,8 +24,19 @@ if (__PROD__) {
   require('offline-plugin/runtime').install()
 }
 
+const Root = (
+  <Provider store={ store }>
+    <div style={ { height: '100%' } }>
+      <Router
+        history={ history }
+        routes={ routes }
+        render={ applyRouterMiddleware(useScroll()) } />
+    </div>
+  </Provider>
+)
+
 // Render the React application to the DOM
 ReactDOM.render(
-  <Root history={ history } routes={ routes } store={ store } />,
+  Root,
   document.getElementById('root')
 )
