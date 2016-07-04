63fffb26d2414d3143264adcd141849734e47f74
DanSnow <dododavid006@gmail.com>
2016-07-04 13:13:19 +0800

    fix(api): Use template interceptor

--- src/lib/api.js
+++ src/lib/api.js
@@ -2,6 +2,7 @@ import rest from 'rest'
 import pathPrefix from 'rest/interceptor/pathPrefix'
 import mime from 'rest/interceptor/mime'
 import errorCode from 'rest/interceptor/errorCode'
+import template from 'rest/interceptor/template'
 import jwtAuth from './interceptors/jwt-auth'
 import headers from './interceptors/headers'
 
@@ -11,6 +12,7 @@ const api = rest
     mime: 'application/json',
     accept: API_HEADER
   })
+  .wrap(template)
   .wrap(jwtAuth)
   .wrap(headers)
   .wrap(errorCode)
