0c0c51f056041b0b24c2388e3613aad7e0fee473
DanSnow <dododavid006@gmail.com>
2016-07-04 23:06:46 +0800

    refactor(routes): computed routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,24 +1,27 @@
 import CoreLayout from 'layouts/CoreLayout/CoreLayout'
+import computedRoutes from './utils/computedRoutes'
 import redirect from './utils/redirect'
 
-export default {
-  path: '/',
-  component: CoreLayout,
-  indexRoute: redirect('/exams'),
-  getChildRoutes(_location, next) {
-    require.ensure([], (require) => {
-      next(null, [
-        require('./SignIn'),
-        require('./SignUp'),
-        require('./Question'),
-        require('./Code'),
-        require('./Submission'),
-        require('./Exam'),
-        require('./DashBoard'),
-        require('./PermissionDenied'),
-        require('./NotFound'),
-        redirect('/page-not-found')
-      ])
-    })
-  }
+export default (store) => {
+  return ({
+    path: '/',
+    component: CoreLayout,
+    indexRoute: redirect('/exams'),
+    getChildRoutes(_location, next) {
+      require.ensure([], (require) => {
+        next(null, computedRoutes(store, [
+          require('./SignIn'),
+          require('./SignUp'),
+          require('./Question'),
+          require('./Code'),
+          require('./Submission'),
+          require('./Exam'),
+          require('./DashBoard'),
+          require('./PermissionDenied'),
+          require('./NotFound'),
+          redirect('/page-not-found')
+        ]))
+      })
+    }
+  })
 }
