48667d716c6a30769f86ba599d95948677018567
DanSnow <dododavid006@gmail.com>
2016-07-24 21:05:10 +0800

    test: Port test

---
+++ src/redux/modules/__tests__/account.spec.js
@@ -0,0 +1,110 @@
+jest.disableAutomock()
+jest.mock('lib/api')
+
+import localStore from 'store'
+import reducer, * as account from 'redux/modules/account'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import api from 'lib/api'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('(Redux) account', () => {
+  describe('(Action Creator) #setLoginState', () => {
+    it('Create "Set login state" action', () => {
+      expect(account.setLoginState()).toEqual({
+        type: account.SET_LOGIN_STATE,
+        payload: false
+      })
+
+      expect(account.setLoginState(true)).toEqual({
+        type: account.SET_LOGIN_STATE,
+        payload: true
+      })
+
+      expect(account.setLoginState(false)).toEqual({
+        type: account.SET_LOGIN_STATE,
+        payload: false
+      })
+    })
+  })
+
+  describe('(Action Creator) #setUserInfo', () => {
+    it('Create "Set user info" action', () => {
+      const info = {
+        username: 'foo',
+        roles: [
+          { id: 1, name: 'admin' }
+        ]
+      }
+
+      expect(account.setUserInfo(info)).toEqual({
+        type: account.SET_USER_INFO,
+        payload: {
+          username: 'foo',
+          roles: [
+            'admin'
+          ]
+        }
+      })
+    })
+  })
+
+  describe('(Action Creator) #clearUser', () => {
+    it('Create "Clear user" action', () => {
+      expect(account.clearUser()).toEqual({
+        type: account.CLEAR_USER,
+        payload: undefined
+      })
+    })
+  })
+
+  describe('(Async Action) #login', () => {
+    describe('When success', () => {
+      it('Will set token', () => {
+        const store = mockStore({
+          account: new account.AccountState()
+        })
+        api.addFakeResponse({
+          path: 'auth/sign-in',
+          entity: 'token'
+        })
+        api.addFakeResponse({
+          path: 'account/profile',
+          entity: {
+            username: 'foo'
+          }
+        })
+
+        return store.dispatch(account.login('user', 'pass')).then((result) => {
+          expect(result).toBe(true)
+          expect(api.mock.calls[0][0].entity).toEqual({ username: 'user', password: 'pass' })
+        })
+      })
+    })
+
+    afterEach(() => {
+      api.clearFakeResponse()
+      localStore.clear()
+    })
+  })
+
+  describe('(Reducer)', () => {
+    it('Configure initialState correct', () => {
+      expect(reducer(undefined, {})).toBe(account.initialState)
+    })
+
+    it('Handle SET_LOGIN_STATE', () => {
+      expect(reducer(account.initialState, {
+        type: account.SET_LOGIN_STATE,
+        payload: true
+      })).toEqualImmutable(new account.AccountState({ valid: true, state: true }))
+
+      expect(reducer(account.initialState, {
+        type: account.SET_LOGIN_STATE,
+        payload: false
+      })).toEqualImmutable(new account.AccountState({ valid: true, state: false }))
+    })
+  })
+})
---
+++ src/redux/modules/__tests__/app.spec.js
@@ -0,0 +1,201 @@
+jest.disableAutomock()
+jest.mock('lib/api')
+
+import { fromJS } from 'immutable'
+import configureStore from 'redux-mock-store'
+import thunk from 'redux-thunk'
+import { RequestStatus } from 'lib/const'
+import reducer, * as app from '../app'
+import { clearExam } from 'redux/modules/exam'
+import { clearQuestion } from 'redux/modules/question'
+import { clearSubmissions } from 'redux/modules/submission'
+import { clearUsers } from 'redux/modules/users'
+import api from 'lib/api'
+
+const middlewares = [thunk]
+const mockStore = configureStore(middlewares)
+
+describe('(Redux) app', () => {
+  describe('(Action Creators) #setStatus', () => {
+    it('Create action to set status', () => {
+      expect(app.setStatus('foo')).toEqual({
+        type: app.SET_STATUS,
+        payload: 'foo'
+      })
+    })
+  })
+
+  describe('(Action Creators) #clearStatus', () => {
+    it('Create action to clear status', () => {
+      expect(app.clearStatus()).toEqual({
+        type: app.CLEAR_STATUS,
+        payload: undefined
+      })
+    })
+  })
+
+  describe('(Action Creators) #setError', () => {
+    it('Create action to set error', () => {
+      expect(app.setError('foo')).toEqual({
+        type: app.SET_ERROR,
+        payload: 'foo'
+      })
+    })
+  })
+
+  describe('(Action Creators) #clearError', () => {
+    it('Create action to clear error', () => {
+      expect(app.clearError()).toEqual({
+        type: app.CLEAR_ERROR,
+        payload: undefined
+      })
+    })
+  })
+
+  describe('(Async Actions) #clearCache', () => {
+    it('Clear all cache', () => {
+      const store = mockStore({})
+
+      store.dispatch(app.clearCache())
+
+      expect(store.getActions()).toContain(clearExam())
+      expect(store.getActions()).toContain(clearQuestion())
+      expect(store.getActions()).toContain(clearUsers())
+      expect(store.getActions()).toContain(clearSubmissions())
+    })
+  })
+
+  describe('(Async Actions) #request', () => {
+    describe('When request success', () => {
+      it('Send request and set success status', () => {
+        const store = mockStore({})
+        api.addFakeResponse({
+          path: 'foo'
+        })
+
+        return store.dispatch(app.request({ path: 'foo' }))
+          .then((result) => {
+            expect(result).toBe(true)
+            expect(store.getActions()).toContain(app.setStatus(RequestStatus.PENDING))
+            expect(store.getActions()).toContain(app.setStatus(RequestStatus.SUCCESS))
+          })
+      })
+    })
+
+    describe('When request fail', () => {
+      it('Send request and set fail status', () => {
+        const store = mockStore({})
+        api.addFakeResponse({
+          path: 'foo',
+          error: {
+            code: 418
+          }
+        })
+
+        return store.dispatch(app.request({ path: 'foo' }))
+          .then((result) => {
+            expect(result).toBe(false)
+            expect(store.getActions()).toContain(app.setStatus(RequestStatus.PENDING))
+            expect(store.getActions()).toContain(app.setStatus(RequestStatus.FAIL))
+          })
+      })
+    })
+
+    afterEach(() => api.clearFakeResponse())
+  })
+
+  describe('(Selector) #isPendingSelector', () => {
+    describe('When is pending', () => {
+      it('Will return true', () => {
+        const initialState = new app.AppStatus({ status: RequestStatus.PENDING })
+        const state = { app: initialState }
+        expect(app.isPendingSelector(state)).toBe(true)
+      })
+    })
+
+    describe('When is not pending', () => {
+      it('Will return false', () => {
+        const initialState = new app.AppStatus({ status: RequestStatus.NONE })
+        const state = { app: initialState }
+        expect(app.isPendingSelector(state)).toBe(false)
+      })
+    })
+  })
+
+  describe('(Selector) #createIsErrorSelector', () => {
+    describe('When is error', () => {
+      it('Will return true', () => {
+        const initialState = new app.AppStatus({ status: RequestStatus.FAIL })
+        const state = { app: initialState }
+        const isErrorSelector = app.createIsErrorSelector()
+        expect(isErrorSelector(state)).toBe(true)
+      })
+    })
+
+    describe('When is not error', () => {
+      it('Will return false', () => {
+        const initialState = new app.AppStatus({ status: RequestStatus.NONE })
+        const state = { app: initialState }
+        const isErrorSelector = app.createIsErrorSelector()
+        expect(isErrorSelector(state)).toBe(false)
+      })
+    })
+  })
+
+  describe('(Selector) #createErrorSelector', () => {
+    describe('When is error', () => {
+      it('Will return error content', () => {
+        const initialState = new app.AppStatus({
+          status: RequestStatus.FAIL,
+          error: fromJS({ messages: 'foo' })
+        })
+        const state = { app: initialState }
+        const errorSelector = app.createErrorSelector()
+        expect(errorSelector(state)).toEqual('foo')
+      })
+    })
+
+    describe('When is not error', () => {
+      it('Will return null', () => {
+        const initialState = new app.AppStatus({
+          status: RequestStatus.NONE,
+          error: fromJS({ messages: 'foo' })
+        })
+        const state = { app: initialState }
+        const errorSelector = app.createErrorSelector()
+        expect(errorSelector(state)).toBeNull()
+      })
+    })
+  })
+
+  describe('(Reducer)', () => {
+    it('Configure initial state correct', () => {
+      expect(reducer(undefined, {})).toEqualImmutable(app.initialState)
+    })
+
+    it('Handle setState action', () => {
+      const expectedState = app.AppStatus({
+        status: 'foo'
+      })
+
+      expect(reducer(app.initialState, {
+        type: app.SET_STATUS,
+        payload: 'foo'
+      })).toEqualImmutable(expectedState)
+    })
+
+    it('Handle clearState action', () => {
+      const initialState = app.AppStatus({
+        status: 'foo'
+      })
+      const expectedState = app.AppStatus({
+        status: 'NONE'
+      })
+      const actualState = reducer(initialState, {
+        type: app.CLEAR_STATUS
+      })
+
+      expect(actualState).toEqualImmutable(expectedState)
+    })
+  })
+})
