db8af66ab76132b597c7e69f2a7cae1494382eb1
DanSnow <dododavid006@gmail.com>
2016-07-30 19:32:10 +0800

    style(utils): Fix indent

--- src/redux/utils/guardRequest.js
+++ src/redux/utils/guardRequest.js
@@ -6,20 +6,20 @@ import handleRequestError from './handleRequestError'
 const guardRequest = (dispatch, reqOpts, handleResult, handleError) => {
   dispatch(setStatus(RequestStatus.PENDING))
   return api(reqOpts)
-  .then(({ entity }) => {
-    if (handleResult) {
-      handleResult(entity)
-    }
-    dispatch(setStatus(RequestStatus.SUCCESS))
-    return true
-  })
-  .catch((error) => {
-    if (handleError) {
-      handleError(error)
-    }
-    handleRequestError(dispatch, error)
-    return false
-  })
+    .then(({ entity }) => {
+      if (handleResult) {
+        handleResult(entity)
+      }
+      dispatch(setStatus(RequestStatus.SUCCESS))
+      return true
+    })
+    .catch((error) => {
+      if (handleError) {
+        handleError(error)
+      }
+      handleRequestError(dispatch, error)
+      return false
+    })
 }
 
 export default guardRequest
