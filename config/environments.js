5ca77a613a5d68dac958901ad5b3811e08b1d9dc
DanSnow <dododavid006@gmail.com>
2016-08-30 13:56:23 +0800

    chore(config): Update config

--- config/environments.js
+++ config/environments.js
@@ -4,7 +4,6 @@ export default {
   production: (config) => ({
     compiler_output_name     : '[name].[hash].js',
     compiler_hash_type       : 'chunkhash',
-    compiler_devtool         : null,
     compiler_stats           : {
       chunks : true,
       chunkModules : true,
