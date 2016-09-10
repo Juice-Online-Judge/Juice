9bf14d5fec5f68fc7da8032de08d6746885e343b
DanSnow <dododavid006@gmail.com>
2016-09-11 00:40:29 +0800

    refactor(routes): Use getComponent

--- src/routes/Exam/index.js
+++ src/routes/Exam/index.js
@@ -3,12 +3,12 @@ import BaseLayout from 'layouts/BaseLayout'
 export default {
   path: 'exams',
   component: BaseLayout,
-  getIndexRoute(_location, next) {
-    require.ensure(['./containers/ExamListView'], () => {
-      next(null, {
-        component: require('./containers/ExamListView')
+  indexRoute: {
+    getComponent(_location, next) {
+      require.ensure(['./containers/ExamListView'], () => {
+        next(null, require('./containers/ExamListView'))
       })
-    })
+    }
   },
   getChildRoutes(_location, next) {
     require.ensure([], (require) => {
--- src/routes/Question/index.js
+++ src/routes/Question/index.js
@@ -3,12 +3,12 @@ import BaseLayout from 'layouts/BaseLayout'
 export default {
   path: 'questions',
   component: BaseLayout,
-  getIndexRoute(_location, next) {
-    require.ensure('./components/QuestionListView', (require) => {
-      next(null, {
-        component: require('./components/QuestionListView')
+  indexRoute: {
+    getComponent(_location, next) {
+      require.ensure('./components/QuestionListView', (require) => {
+        next(null, require('./components/QuestionListView'))
       })
-    })
+    }
   },
   getChildRoutes(_location, next) {
     require.ensure([], (require) => {
