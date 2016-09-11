4a1df5439eecad2b318cc576162c342eb60df768
DanSnow <dododavid006@gmail.com>
2016-09-11 23:11:21 +0800

    refactor(BaseLayout): Direct return children

--- src/layouts/BaseLayout.js
+++ src/layouts/BaseLayout.js
@@ -1,10 +1,6 @@
-import React from 'react'
+import { Children } from 'react'
 import setDisplayName from 'recompose/setDisplayName'
 
-const BaseLayout = setDisplayName('BaseLayout')(({ children }) => (
-  <div>
-    { children }
-  </div>
-))
+const BaseLayout = setDisplayName('BaseLayout')(({ children }) => Children.only(children))
 
 export default BaseLayout
