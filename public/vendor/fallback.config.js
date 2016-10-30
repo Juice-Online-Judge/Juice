51aa93ff3dec891ac75f34deba100db3e46dfeab
DanSnow <dododavid006@gmail.com>
2016-10-30 13:49:53 +0800

    chore(git): Update gitignore

---
+++ public/vendor/fallback.config.js
@@ -0,0 +1,10 @@
+fallback.load({
+  'String.prototype.trim': [
+    'https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-shim.min.js',
+    '/vendor/es5-shim/es5-shim.js'
+  ],
+  'Object.defineProperty': [
+    'https://cdnjs.cloudflare.com/ajax/libs/es5-shim/4.5.7/es5-sham.min.js',
+    '/vendor/es5-shim/es5-sham.js'
+  ]
+})
