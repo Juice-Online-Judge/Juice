3384b65715493cbfe6d4beb93c9525a551014bd3
DanSnow <dododavid006@gmail.com>
2016-11-15 23:02:00 +0800

    test(env): Wrap jasmine-immutable-matcher to work for jest

--- tests/jest-env.js
+++ tests/jest-env.js
@@ -1,6 +1,17 @@
 import 'jest-enzyme/lib'
 import * as jasmineImmutable from 'jasmine-immutable-matchers'
+import mapValues from 'lodash/mapValues'
 
-beforeEach(() => {
-  jasmine.addMatchers(jasmineImmutable)
+const jestImmutable = mapValues(jasmineImmutable, (matcherFactory) => {
+  const matcher = matcherFactory()
+  return function() {
+    const { isNot } = this
+    const { pass, message } = matcher[isNot ? 'negativeCompare' : 'compare'](...arguments)
+    return {
+      pass: Boolean(pass),
+      message
+    }
+  }
 })
+
+expect.extend(jestImmutable)
