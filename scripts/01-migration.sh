4b55aed96d1b9aacc076a871cbc7fbf0002c39a5
DanSnow <dododavid006@gmail.com>
2016-12-05 19:09:03 +0800

    chore(scripts): Check db status [ci skip]

--- scripts/01-migration.sh
+++ scripts/01-migration.sh
@@ -2,5 +2,12 @@
 
 cd /var/www/html
 
+until mysqladmin -h "$DB_HOST" -u "$DB_USER" -p"$DB_PASSWORD" version; do
+  >&2 echo "Database is unavailable - sleeping"
+  sleep 1
+done
+
+>&2 echo "Database is up - start migration"
+
 php artisan migrate -n --force
 
