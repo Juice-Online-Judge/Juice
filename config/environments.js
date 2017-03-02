1141846d389f30dbc724dfcf9de0079abcdfb893
DanSnow <dododavid006@gmail.com>
2017-03-02 20:12:04 +0800

    chore(config): Enable hash

--- config/environments.js
+++ config/environments.js
@@ -2,7 +2,8 @@
 
 export default {
   production: (config) => ({
-    compiler_chunk_name     : '[name].[hash].js',
+    compiler_output_name     : '[name].[hash].js',
+    compiler_chunk_name     : '[name].[chunkhash].js',
     compiler_hash_type       : 'chunkhash',
     compiler_stats           : {
       chunks : true,
