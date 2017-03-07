54d812fd139c797e0bb3345a4e90a3aff16375b0
DanSnow <dododavid006@gmail.com>
2017-03-07 22:03:25 +0800

    fix: Fix for store v2

--- src/lib/interceptors/jwt-auth.js
+++ src/lib/interceptors/jwt-auth.js
@@ -1,8 +1,8 @@
 import store from 'store'
 
 const jwtAuth = (request) => {
-  if (store.has('juice-token')) {
-    const token = store.get('juice-token')
+  const token = store.get('juice-token')
+  if (token !== undefined) {
     request.headers = request.headers || {}
     request.headers['Authorization'] = `Bearer ${token}`
   }
