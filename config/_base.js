4912e2ba155a46c1b634fbccb4cdd2acc21762bb
DanSnow <dododavid006@gmail.com>
2017-03-17 22:20:19 +0800

    chore(config): Update vendor

--- config/_base.js
+++ config/_base.js
@@ -1,44 +1,44 @@
 /* eslint key-spacing:0 spaced-comment:0 */
 import _debug from 'debug'
 import path from 'path'
-import { argv } from 'yargs'
+import {argv} from 'yargs'
 
 const debug = _debug('app:config:_base')
 const config = {
-  env : process.env.NODE_ENV || 'development',
+  env: process.env.NODE_ENV || 'development',
 
   // ----------------------------------
   // Project Structure
   // ----------------------------------
-  path_base  : path.resolve(__dirname, '../'),
-  dir_client : 'src',
-  dir_dist   : 'public',
-  dir_server : 'server',
-  dir_test   : 'tests',
+  path_base: path.resolve(__dirname, '../'),
+  dir_client: 'src',
+  dir_dist: 'public',
+  dir_server: 'server',
+  dir_test: 'tests',
 
   // ----------------------------------
   // Server Configuration
   // ----------------------------------
-  server_host : process.env.APP_URL || 'localhost',
-  server_port : process.env.APP_PORT || 80,
+  server_host: process.env.APP_URL || 'localhost',
+  server_port: process.env.APP_PORT || 80,
 
   // ----------------------------------
   // Compiler Configuration
   // ----------------------------------
-  compiler_output_name     : '[name].js',
-  compiler_chunk_name     : '[name].js',
-  compiler_css_modules     : true,
-  compiler_devtool         : 'source-map',
-  compiler_hash_type       : 'hash',
-  compiler_fail_on_warning : false,
-  compiler_quiet           : false,
-  compiler_public_path     : '/',
-  compiler_stats           : {
-    chunks : false,
-    chunkModules : false,
-    colors : true
+  compiler_output_name: '[name].js',
+  compiler_chunk_name: '[name].js',
+  compiler_css_modules: true,
+  compiler_devtool: 'source-map',
+  compiler_hash_type: 'hash',
+  compiler_fail_on_warning: false,
+  compiler_quiet: false,
+  compiler_public_path: '/',
+  compiler_stats: {
+    chunks: false,
+    chunkModules: false,
+    colors: true
   },
-  compiler_vendor : [
+  compiler_vendor: [
     'axios',
     'classnames',
     'decko',
@@ -49,12 +49,12 @@ const config = {
     'react',
     'react-redux',
     'react-router',
+    'react-router-dom',
     'redux',
     'redux-actions',
     'redux-thunk',
     'react-flexbox-grid',
     'react-router-redux',
-    'react-router-scroll',
     'react-tap-event-plugin',
     'reselect',
     'recompose',
@@ -66,11 +66,8 @@ const config = {
   // ----------------------------------
   // Test Configuration
   // ----------------------------------
-  coverage_enabled   : !argv.watch,
-  coverage_reporters : [
-    { type : 'text-summary' },
-    { type : 'html', dir : 'coverage' }
-  ]
+  coverage_enabled: !argv.watch,
+  coverage_reporters: [{type: 'text-summary'}, {type: 'html', dir: 'coverage'}]
 }
 
 /************************************************
@@ -93,17 +90,17 @@ const apiVersion = process.env.API_VERSION
 const apiHeader = `application/${apiSubtree}.${apiSubtype}.${apiVersion}+json`
 
 config.globals = {
-  'process.env'  : {
-    'NODE_ENV' : JSON.stringify(config.env)
+  'process.env': {
+    NODE_ENV: JSON.stringify(config.env)
   },
-  'NODE_ENV'     : config.env,
-  'API_HEADER'   : JSON.stringify(apiHeader),
-  '__DEV__'      : config.env === 'development',
-  '__PROD__'     : config.env === 'production',
-  '__TEST__'     : config.env === 'test',
-  '__DEBUG__'    : config.env === 'development' && !argv.no_debug,
-  '__COVERAGE__' : !argv.watch && config.env === 'test',
-  '__BASENAME__' : JSON.stringify(process.env.BASENAME || '')
+  NODE_ENV: config.env,
+  API_HEADER: JSON.stringify(apiHeader),
+  __DEV__: config.env === 'development',
+  __PROD__: config.env === 'production',
+  __TEST__: config.env === 'test',
+  __DEBUG__: config.env === 'development' && !argv.no_debug,
+  __COVERAGE__: !argv.watch && config.env === 'test',
+  __BASENAME__: JSON.stringify(process.env.BASENAME || '')
 }
 
 // ------------------------------------
@@ -111,16 +108,15 @@ config.globals = {
 // ------------------------------------
 const pkg = require('../package.json')
 
-config.compiler_vendor = config.compiler_vendor
-  .filter(dep => {
-    if (pkg.dependencies[dep]) return true
+config.compiler_vendor = config.compiler_vendor.filter(dep => {
+  if (pkg.dependencies[dep]) return true
 
-    debug(
-      `Package "${dep}" was not found as an npm dependency in package.json; ` +
+  debug(
+    `Package "${dep}" was not found as an npm dependency in package.json; ` +
       `it won't be included in the webpack vendor bundle.\n` +
       'Consider removing it from vendor_dependencies in ~/config/_base.js'
-    )
-  })
+  )
+})
 
 // ------------------------------------
 // Utilities
@@ -133,8 +129,8 @@ config.utils_paths = (() => {
 
   return {
     base,
-    client : base.bind(null, config.dir_client),
-    dist   : base.bind(null, config.dir_dist)
+    client: base.bind(null, config.dir_client),
+    dist: base.bind(null, config.dir_dist)
   }
 })()
 
