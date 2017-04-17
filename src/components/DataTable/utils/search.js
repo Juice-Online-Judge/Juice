0c524cb3ce0260a51623cc3bc6c65485df13fe73
DanSnow <dododavid006@gmail.com>
2017-04-18 00:15:08 +0800

    refactor: Use #some

--- src/components/DataTable/utils/search.js
+++ src/components/DataTable/utils/search.js
@@ -1,22 +1,15 @@
-const regexMatchFactory = (filter) => {
+const regexMatchFactory = filter => {
   const regex = new RegExp(filter, 'i')
-  return (item) => String(item).match(regex)
+  return item => String(item).match(regex)
 }
 
-const search = ({key, filter, data, matchFactory = regexMatchFactory}) => {
+const search = ({ key, filter, data, matchFactory = regexMatchFactory }) => {
   if (filter.length < 1) return data
 
   const match = matchFactory(filter)
   const keys = typeof key === 'string' ? [key] : key
 
-  return data.filter((item) => {
-    for (let i = 0; i < keys.length; i += 1) {
-      if (match(item[keys[i]])) {
-        return true
-      }
-    }
-    return false
-  })
+  return data.filter(item => keys.some(key => match(item[key])))
 }
 
 export default search
