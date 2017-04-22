8163e70497e918298645376a9a102c6b64868b46
DanSnow <dododavid006@gmail.com>
2017-04-22 15:29:59 +0800

    chore: Refactor common code

---
+++ bin/run-compiler.js
@@ -0,0 +1,24 @@
+import path from 'path'
+import fs from 'fs'
+import pify from 'pify'
+import webpackCompiler from '../build/webpack-compiler'
+import webpackConfig from '../build/webpack.config'
+import config from '../config'
+
+export default async function({ debug, compilerConfig = {}, json = false }) {
+  try {
+    debug('Run compiler')
+    const stats = await webpackCompiler({...webpackConfig, ...compilerConfig})
+    if (stats.warnings.length && config.compiler_fail_on_warning) {
+      debug('Config set to fail on warning, exiting with status code "1".')
+      process.exit(1)
+    }
+    if (json) {
+      await pify(fs.writeFile)(path.join(__dirname, '..', 'stats.json'), JSON.stringify(stats))
+      debug('Write state to state.json')
+    }
+  } catch (e) {
+    debug('Compiler encountered an error.', e)
+    process.exit(1)
+  }
+}
--- bin/watch.js
+++ bin/watch.js
@@ -1,21 +1,8 @@
 import _debug from 'debug'
-import webpackCompiler from '../build/webpack-compiler'
-import webpackConfig from '../build/webpack.config'
-import config from '../config'
+import runCompiler from './run-compiler'
 
 const debug = _debug('app:bin:watch')
-;(async function() {
-  try {
-    debug('Run compiler')
-    const stats = await webpackCompiler(
-      Object.assign(webpackConfig, {watch: true})
-    )
-    if (stats.warnings.length && config.compiler_fail_on_warning) {
-      debug('Config set to fail on warning, exiting with status code "1".')
-      process.exit(1)
-    }
-  } catch (e) {
-    debug('Compiler encountered an error.', e)
-    process.exit(1)
-  }
-})()
+runCompiler({
+  debug,
+  compilerConfig: { watch: true }
+})
