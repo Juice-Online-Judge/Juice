c2ea52386ac898f3f04620ee11fa251234181b44
DanSnow <dododavid006@gmail.com>
2017-04-24 18:46:24 +0800

    fix: Fix style

--- src/routes/Submission/components/SubmissionView/SubmissionView.js
+++ src/routes/Submission/components/SubmissionView/SubmissionView.js
@@ -1,35 +1,35 @@
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
+import { connect } from 'react-redux'
 
 import Inset from 'layouts/Inset'
 import LoadingContainer from 'containers/LoadingContainer'
 import SubmissionList from 'components/SubmissionList'
 
-import {fetchSubmissions} from 'redux/modules/submission'
+import { fetchSubmissions } from 'redux/modules/submission'
 
 export class SubmissionView extends Component {
   componentDidMount() {
-    this.props.fetchSubmissions({force: true})
+    this.props.fetchSubmissions({ force: true })
   }
 
   render() {
-    const {submission} = this.props
+    const { submission } = this.props
     return (
-      <LoadingContainer>
-        <Inset>
+      <Inset>
+        <LoadingContainer>
           <SubmissionList submission={ submission } />
-        </Inset>
-      </LoadingContainer>
+        </LoadingContainer>
+      </Inset>
     )
   }
 
   static propTypes = {
     submission: PropTypes.object.isRequired,
     fetchSubmissions: PropTypes.func.isRequired
-  }
+  };
 }
 
-export default connect(state => ({submission: state.submission}), {
+export default connect(state => ({ submission: state.submission }), {
   fetchSubmissions
 })(SubmissionView)
