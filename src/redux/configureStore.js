a3e3ffabd0ad0a812fe59c83c50ff4b2db196c8b
DanSnow <dododavid006@gmail.com>
2017-04-25 15:33:08 +0800

    fix(store): Allow access store in production mode

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -4,17 +4,13 @@ import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
 export default function configureStore ({initialState = {}, browserHistory}) {
-  // Compose final middleware and use devtools in debug environment
   let middleware = applyMiddleware(thunk, routerMiddleware(browserHistory))
-  if (__DEBUG__) {
-    const devToolsExtension = window.devToolsExtension
+  const {devToolsExtension} = window
 
-    if (typeof devToolsExtension === 'function') {
-      middleware = compose(middleware, window.devToolsExtension())
-    }
+  if (typeof devToolsExtension === 'function') {
+    middleware = compose(middleware, window.devToolsExtension())
   }
 
-  // Create final store and subscribe router in debug env ie. for devtools
   const store = createStore(rootReducer, initialState, middleware)
 
   return store
