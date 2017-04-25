25ee4be177f6e6cf8dc59fd422c54b5446034461
DanSnow <dododavid006@gmail.com>
2017-04-25 22:23:26 +0800

    refactor: Use helper

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,17 +1,16 @@
-import {applyMiddleware, compose, createStore} from 'redux'
+import {applyMiddleware, createStore} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension/logOnlyInProduction'
 import {routerMiddleware} from 'react-router-redux'
 import thunk from 'redux-thunk'
 import rootReducer from './rootReducer'
 
 export default function configureStore ({initialState = {}, browserHistory}) {
-  let middleware = applyMiddleware(thunk, routerMiddleware(browserHistory))
-  const {devToolsExtension} = window
-
-  if (typeof devToolsExtension === 'function') {
-    middleware = compose(middleware, window.devToolsExtension())
-  }
-
-  const store = createStore(rootReducer, initialState, middleware)
+  const middleware = applyMiddleware(thunk, routerMiddleware(browserHistory))
+  const store = createStore(
+    rootReducer,
+    initialState,
+    composeWithDevTools(middleware)
+  )
 
   return store
 }
