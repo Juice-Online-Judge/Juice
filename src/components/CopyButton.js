589447ff89505dd58bd2ff601b54ee699fd550ab
DanSnow <dododavid006@gmail.com>
2017-06-07 18:21:02 +0800

    refactor: Use copy-text-to-clipboard

--- src/components/CopyButton.js
+++ src/components/CopyButton.js
@@ -2,26 +2,27 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
 import FlatButton from 'material-ui/FlatButton'
-import ClipboardButton from 'react-clipboard.js'
+import copy from 'copy-text-to-clipboard'
 
 import {setOpen} from 'redux/modules/message'
 import MessageContainer from '../containers/MessageContainer'
 
 export class CopyButton extends Component {
-  handleCopySuccess = () => {
-    this.props.setOpen(true)
+  handleClick = () => {
+    if (copy(this.props.text)) {
+      this.props.setOpen(true)
+    }
   }
 
   render () {
     const {text} = this.props
     return (
       <MessageContainer message='Copy success'>
-        <ClipboardButton
-          component='a'
-          data-clipboard-text={text}
-          onSuccess={this.handleCopySuccess}>
-          <FlatButton label='Copy' primary disabled={!text} />
-        </ClipboardButton>
+        <FlatButton
+          onTouchTap={this.handleClick}
+          label='Copy'
+          primary
+          disabled={!text} />
       </MessageContainer>
     )
   }
